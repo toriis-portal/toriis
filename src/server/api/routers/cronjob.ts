ec148c7baf8e1b4515d03ef1652348dcb39e0620
daniel-moon32 <dm32@illinois.edu>
2023-02-20 17:19:26 -0600

    Create workflow and template endpoint

---
+++ src/server/api/routers/cronjob.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod'
+
+import { createTRPCRouter, protectedProcedure } from '../trpc'
+
+export const ESGCronJobProcedure = createTRPCRouter({
+  ESGCronJob: protectedProcedure.query(({ ctx }) => {
+    const companies = ctx.prisma.company.findMany({
+      take: 50,
+    })
+
+    // companies.forEach(async (company) => {
+    //   const { data } = await fetch(`https://esgapiservice.com/api/authorization/search?q=msft,v,intc&token=${secrets.ESG_API_KEY}$`)
+
+    // return ctx.prisma.eSG.create({
+    //     data: {})
+  }),
+})
