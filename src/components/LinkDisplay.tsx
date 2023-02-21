a3986d9e4350df972f18473e3e7bebbde0fc9011
Liza George <liza@chiramattel.com>
2023-02-20 18:32:43 -0600

    Adding and styling links

--- src/components/LinkDisplay.tsx
+++ src/components/LinkDisplay.tsx
@@ -1,7 +1,27 @@
 import type { FC } from 'react'
 
+import data from '../info/instDivestment.json'
+
+interface Pair {
+  text: string
+  link: string
+}
+const links: Pair[] = data.links as Pair[]
+
 const LinkDisplay: FC = () => {
-  return <h1>hi babes</h1>
+  return (
+    <div className="flex w-full flex-col items-center justify-center rounded-md bg-darkTeal py-12 px-12 text-white">
+      <h1>Institutional Divestment Links</h1>
+
+      <div className="flex flex-wrap justify-center  text-sm leading-loose underline ">
+        {links.map((pair: Pair, index: number) => (
+          <a href={pair.link} key={index} className="mx-2">
+            {pair.text}
+          </a>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default LinkDisplay
