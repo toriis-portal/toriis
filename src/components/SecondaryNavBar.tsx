5b333b64527de1db5537b9fe82146d47d792dca2
eeshabarua <eesha.barua@gmail.com>
2023-02-23 17:30:06 -0600

    All done

--- src/components/SecondaryNavBar.tsx
+++ src/components/SecondaryNavBar.tsx
@@ -6,13 +6,13 @@ import SampleSection from '../sections/SampleSection'
 const NavBar: FC = () => {
   return (
     <>
-      <nav className="sticky top-0 w-full justify-around bg-darkTeal px-2 py-2.5">
-        <div className="container mx-auto flex flex-wrap">
+      <nav className="sticky top-0 justify-evenly bg-darkTeal py-2.5">
+        <div className="container mx-auto">
           <div className="md:flex">
-            <ul className="flex p-4 text-white md:space-x-8 md:text-sm ">
+            <ul className="flex space-x-8 p-4 text-sm text-white ">
               <li>
                 <Link
-                  activeClass=""
+                  activeClass="active"
                   to="container1"
                   spy={true}
                   smooth={true}
