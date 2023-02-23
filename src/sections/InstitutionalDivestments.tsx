ca524614718c34e862096582cad9d3271fdb47d6
anthony <anthony.cruzmacedo@protonmail.com>
2023-02-22 22:33:06 -0600

    Move DivestmentList component to InstitutionalDivestments in sections folder

---
+++ src/sections/InstitutionalDivestments.tsx
@@ -0,0 +1,41 @@
+import type { FC } from 'react'
+
+import HomeData from '../info/home.json'
+import HighlightedTitle from '../components/HighlightedTitle'
+import ListItem from '../components/ListItem'
+
+interface props {
+  title: string
+}
+
+interface JSONData {
+  [x: string]: string
+}
+
+const InstitutionalDivestments: FC<props> = ({ title }) => {
+  const strings: object = HomeData['divestment-strings'] as object
+
+  return (
+    <>
+      <table className="flex-column items-start">
+        <tbody>
+          <tr>
+            <th className="pb-10 text-left text-2xl font-bold">
+              <HighlightedTitle title={title} />
+            </th>
+          </tr>
+          {strings &&
+            Object.keys(strings)?.map((k) => (
+              <ListItem
+                key={k}
+                listVal={k}
+                listContent={(strings as JSONData)[k] ?? ''}
+              />
+            ))}
+        </tbody>
+      </table>
+    </>
+  )
+}
+
+export default InstitutionalDivestments
