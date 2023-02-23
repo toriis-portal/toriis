c5ecaab78ceb2cde88414fdb48a533fd4f351442
Liza George <liza@chiramattel.com>
2023-02-23 11:14:22 -0600

    Add offset and hardcoded-sized dropshadow

--- src/components/LinkDisplay.tsx
+++ src/components/LinkDisplay.tsx
@@ -10,10 +10,16 @@ const links: Pair[] = data.links as Pair[]
 
 const LinkDisplay: FC = () => {
   return (
-    <div className="flex w-full flex-col items-center justify-center rounded-md bg-darkTeal py-12 px-12 text-white">
-      <h1>Institutional Divestment Links</h1>
+    <div className="mt-8 flex w-full flex-col items-center justify-center rounded-md bg-darkTeal py-16 px-12 text-white">
+      <div className="z-10 -mt-22 rounded-full border-2 border-colbalt bg-white px-8 py-2">
+        <h1 className="text-lg font-semibold text-darkTeal">
+          Institutional Divestment Links
+        </h1>
+      </div>
+      <div className="-mt-9 -ml-2 h-10 w-76 rounded-full bg-colbalt"></div>
+      {/* TODO: should i be using placeholder text instead of hardcoding this height and width? */}
 
-      <div className="flex flex-wrap justify-center  text-sm leading-loose underline ">
+      <div className="mt-8 flex flex-wrap  justify-center text-sm leading-loose underline">
         {links.map((pair: Pair, index: number) => (
           <a
             href={pair.link}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        22: '88px',
+        76: '314px',
+      },
+    },
     colors: {
       colbalt: '#0F81E8',
       lightBlue: '#E6F0FA',
