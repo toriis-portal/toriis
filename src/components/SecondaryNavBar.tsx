814917fbf7bfa1466aa2288d4e7f9e784c2189f1
eeshabarua <eesha.barua@gmail.com>
2023-02-23 17:07:55 -0600

    Latest Update

--- src/components/SecondaryNavBar.tsx
+++ src/components/SecondaryNavBar.tsx
@@ -6,15 +6,18 @@ import SampleSection from '../sections/SampleSection'
 const NavBar: FC = () => {
   return (
     <>
-      <nav className="navbar-fixed-top w-full justify-around bg-darkTeal px-2 py-2.5">
+      <nav className="sticky top-0 w-full justify-around bg-darkTeal px-2 py-2.5">
         <div className="container mx-auto flex flex-wrap">
           <div className="md:flex">
             <ul className="flex p-4 text-white md:space-x-8 md:text-sm ">
               <li>
                 <Link
-                  activeClass="active"
+                  activeClass=""
                   to="container1"
-                  className="no-underline hover:underline"
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                  className="no-underline hover:underline hover:underline-offset-8"
                 >
                   Our Requests
                 </Link>
@@ -23,23 +26,44 @@ const NavBar: FC = () => {
                 <Link
                   activeClass="active"
                   to="container2"
-                  className="no-underline hover:underline"
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                  className="no-underline hover:underline hover:underline-offset-8"
                 >
                   Institutional Divestment
                 </Link>
               </li>
               <li>
-                <Link to="container3" className="no-underline hover:underline">
+                <Link
+                  to="container3"
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                  className="no-underline hover:underline hover:underline-offset-8"
+                >
                   Refute UIS Response
                 </Link>
               </li>
               <li>
-                <Link to="container4" className="no-underline hover:underline">
+                <Link
+                  to="container4"
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                  className="no-underline hover:underline hover:underline-offset-8"
+                >
                   UIUC System
                 </Link>
               </li>
               <li>
-                <Link to="container5" className="no-underline hover:underline">
+                <Link
+                  to="container5"
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                  className="no-underline hover:underline hover:underline-offset-8"
+                >
                   Divestment History
                 </Link>
               </li>
@@ -47,11 +71,21 @@ const NavBar: FC = () => {
           </div>
         </div>
       </nav>
-      <SampleSection name="container1" text="Our Requests" />
-      <SampleSection name="container2" text="Institutional Divestment" />
-      <SampleSection name="container3" text="Refute UIS Response" />
-      <SampleSection name="container4" text="UIUC System" />
-      <SampleSection name="container5" text="Divestment History" />
+      <div id="container1" className="pt-16">
+        <SampleSection text="Our Requests" />
+      </div>
+      <div id="container2" className="pt-16">
+        <SampleSection text="Institutional Divestment" />
+      </div>
+      <div id="container3" className="pt-16">
+        <SampleSection text="Refute UIS Response" />
+      </div>
+      <div id="container4" className="pt-16">
+        <SampleSection text="UIUC System" />
+      </div>
+      <div id="container5" className="pt-16">
+        <SampleSection text="Divestment History" />
+      </div>
       <a onClick={() => scroll.scrollToTop()}>To the top!</a>
     </>
   )
--- src/sections/SampleSection.tsx
+++ src/sections/SampleSection.tsx
@@ -3,11 +3,10 @@ import type { FC } from 'react'
 import SampleText from '../components/SampleText'
 
 interface InputProps {
-  name: string
   text: string
 }
 
-const SampleSection: FC<InputProps> = ({ name, text }) => {
+const SampleSection: FC<InputProps> = ({ text }) => {
   return (
     <>
       <div className="h-screen border-spacing-1 bg-slate-200 text-6xl">
