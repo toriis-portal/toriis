a6cbb4c9717903f688fc49c004090e89f8a02e6b
Liza George <liza@chiramattel.com>
2023-02-24 16:17:36 -0600

    Nit fixes

--- src/components/LinkDisplay.tsx
+++ src/components/LinkDisplay.tsx
@@ -14,19 +14,14 @@ const LinkDisplay: FC = () => {
   return (
     <div className="mt-12 flex w-full flex-col items-center rounded-md bg-darkTeal px-6 pt-6 pb-16 text-white">
       <div className="relative -top-12 z-10 rounded-full border-2 border-colbalt bg-white px-16 py-2 shadow-cobalt">
-        <h1 className="text-3xl text-darkTeal ">
+        <p className="text-3xl text-darkTeal ">
           Institutional Divestment Links
-        </h1>
+        </p>
       </div>
       <div className="flex flex-wrap justify-center leading-loose underline">
         {links.map((pair: Pair, index: number) => (
           <div key={index} className="mx-2 inline-block">
-            <Link
-              href={pair.link}
-              // to open link in new tab:
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <Link href={pair.link} target="_blank" rel="noopener noreferrer">
               {pair.text}
             </Link>
             <ArrowUpRightIcon className="ml-2 inline h-4 w-4 text-white" />
