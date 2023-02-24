43c2f5851755047981caac9f9f17936b0c7ea201
Liza George <liza@chiramattel.com>
2023-02-23 18:29:22 -0600

    Use heroicon for arrow icon next to link

--- src/components/LinkDisplay.tsx
+++ src/components/LinkDisplay.tsx
@@ -1,5 +1,6 @@
 import type { FC } from 'react'
 import Link from 'next/link'
+import { ArrowUpRightIcon } from '@heroicons/react/24/solid'
 
 import data from '../info/home.json'
 
@@ -11,7 +12,7 @@ const links = (data.instDivestment as { links: Pair[] }).links
 
 const LinkDisplay: FC = () => {
   return (
-    <div className="mt-12 flex w-full flex-col items-center rounded-md bg-darkTeal px-10 pt-6 pb-16 text-white">
+    <div className="mt-12 flex w-full flex-col items-center rounded-md bg-darkTeal px-6 pt-6 pb-16 text-white">
       <div className="relative -top-12 z-10 rounded-full border-2 border-colbalt bg-white px-16 py-2 shadow-cobalt">
         <h1 className="text-3xl text-darkTeal ">
           Institutional Divestment Links
@@ -19,16 +20,17 @@ const LinkDisplay: FC = () => {
       </div>
       <div className="flex flex-wrap justify-center leading-loose underline">
         {links.map((pair: Pair, index: number) => (
-          <Link
-            href={pair.link}
-            key={index}
-            // to open link in new tab:
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mx-2"
-          >
-            {pair.text}
-          </Link>
+          <div key={index} className="mx-2 inline-block">
+            <Link
+              href={pair.link}
+              // to open link in new tab:
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {pair.text}
+            </Link>
+            <ArrowUpRightIcon className="ml-2 inline h-4 w-4 text-white" />
+          </div>
         ))}
       </div>
     </div>
