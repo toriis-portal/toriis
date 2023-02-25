998d33e5dd24148b70d56baaa6e85804d5b73b9c
Alex Masgras <masgras2@illinois.edu>
2023-02-25 12:11:36 -0600

    Clean up navbar code

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,7 @@
 import type { FC } from 'react'
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 import { NavButton } from '../components'
 
@@ -8,23 +9,19 @@ import toriisimg from './/../img/toriis.png'
 
 const NavBar: FC = () => {
   return (
-    <nav className="bg-white">
-      <div className="flex flex-row items-center justify-between px-10">
-        <a href="link to main page" className="justify-start">
-          <Image
-            src={toriisimg as HTMLImageElement}
-            className=""
-            alt="Toriis Logo"
-            height="100"
-            width="100"
-          />
-        </a>
-        <div className="justify-end">
-          <div className="flex flex-row pb-1">
-            <NavButton title="Home" link="/" />
-            <NavButton title="Fossil Fuel" link="/fossil-fuel" />
-            <NavButton title="Learn about Investments" link="/investments" />
-          </div>
+    <nav className="flex h-[120px] flex-row items-center justify-between bg-white px-12">
+      <Link href="/" className="h-full w-auto justify-start">
+        <Image
+          src={toriisimg as HTMLImageElement}
+          className="h-full w-auto object-contain"
+          alt="Toriis Logo"
+        />
+      </Link>
+      <div className="justify-end">
+        <div className="flex flex-row pb-1">
+          <NavButton title="Home" link="/" />
+          <NavButton title="Fossil Fuel" link="/fossil-fuel" />
+          <NavButton title="Learn about Investments" link="/investments" />
         </div>
       </div>
     </nav>
