f4e15257f74affe566be2ba9dce793bac4187c57
Zora Zhang <zorazrr@gmail.com>
2023-02-24 21:16:57 -0600

    Add clsx

--- src/components/NavButton.tsx
+++ src/components/NavButton.tsx
@@ -2,6 +2,7 @@ import type { FC } from 'react'
 import React from 'react'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
+import clsx from 'clsx'
 
 interface NavButtonProps {
   title: string
@@ -9,19 +10,21 @@ interface NavButtonProps {
 }
 
 const NavButton: FC<NavButtonProps> = ({ title, link }) => {
+  const isActive = useRouter().pathname === link
+
   return (
     <div className="flex flex-col gap-1 bg-white">
       <Link
         href={link}
-        className="m-0 px-4 text-center font-klima font-medium text-black"
+        className={clsx(
+          'm-0 px-4 text-center font-klima font-medium text-black',
+          {
+            'border-b-4 border-cobalt': isActive,
+          },
+        )}
       >
         {title}
       </Link>
-      {useRouter().pathname === link ? (
-        <div className=" border-2 border-cobalt" />
-      ) : (
-        <div className=" border-2 border-white" />
-      )}
     </div>
   )
 }
