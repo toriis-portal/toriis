2039c5f3ac745958bf58e2c46b711e2022b99705
21xiaofanli <91353008+21xiaofanli@users.noreply.github.com>
2023-02-25 20:05:09 -0600

    Primary Button Component (#28)
    * changes didnt show up
    
    * color issue
    
    * temp fix
    
    * added over and link
    
    * Add clsx (#24)
    
    * Fixed the previous issue and all the minor fixes
    
    * Deleted the button from index.tsx
    
    * Add Top-Level Nav Bar (#18)
    
    * Start to build nav button
    
    * Style nav button
    
    * Add Klima font
    
    * Build and add navbar component
    
    * Fix lint errors
    
    * Place navbar on app home, Use router for button style
    
    * Make logo smaller
    
    * Clean up code, Use nextjs Links for buttons
    
    * Make final changes
    
    * Add clsx
    
    * Implement cslx
    
    * Clean up navbar code
    
    ---------
    
    Co-authored-by: Zora Zhang <zorazrr@gmail.com>
    
    * Cronjob to pull from ESG (#14)
    
    * Create workflow and template endpoint
    
    * update cronjob to test on pr
    
    * finish code to push to esg
    
    * Finish index tracking
    
    * Update error checking and refactor
    
    * Add authorization key
    
    * Update max api calls
    
    * Fix types and cronjob
    
    * Add font
    
    * Adjust interface naming
    
    * Change formatting to make it uniform
    
    ---------
    
    Co-authored-by: Zora Zhang <zorazrr@gmail.com>
    Co-authored-by: Alex Masgras <111911351+masgras2@users.noreply.github.com>
    Co-authored-by: Daniel Moon <36148173+daniel-moon32@users.noreply.github.com>

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
-import AuthButton from './AuthButton'
-import NavButton from './NavButton'
+import AuthButton from './Buttons/AuthButton'
+import NavButton from './Buttons/NavButton'
 import NavBar from './NavBar'
+import PrimaryButton from './Buttons/PrimaryButton'
 
-export { AuthButton, NavButton, NavBar }
+export { AuthButton, NavButton, NavBar, PrimaryButton }
