fc9efdc01c18aba3d2ab05a224e05fca4f4065e6
eeshabarua <eesha.barua@gmail.com>
2023-02-27 22:42:39 -0600

    Add Map Functionality

--- src/components/Nav/SecondaryNavBar.tsx
+++ src/components/Nav/SecondaryNavBar.tsx
@@ -2,73 +2,34 @@ import type { FC } from 'react'
 import { Link } from 'react-scroll'
 import clsx from 'clsx'
 
+import data from '../../info/home.json'
+
 const SecondaryNavBar: FC = () => {
   const navStyle = clsx(
     'no-underline hover:decoration-clementine hover:underline hover:underline-offset-8 hover:decoration-[0.2rem]',
   )
+  const sourceText: { path: string; text: string }[] = data.secondaryNavText
   return (
     <nav className="center sticky top-0 z-50 bg-darkTeal py-2.5">
       <ul className="flex cursor-pointer flex-row items-center justify-center space-x-8 p-4 text-white">
-        <li>
-          <Link
-            activeClass="active"
-            to="ourRequests"
-            spy={true}
-            smooth={true}
-            duration={500}
-            className={navStyle}
-          >
-            Our Requests
-          </Link>
-        </li>
-        <li>
-          <Link
-            activeClass="active"
-            to="institutionalDivestment"
-            spy={true}
-            smooth={true}
-            duration={500}
-            className={navStyle}
-          >
-            Institutional Divestment
-          </Link>
-        </li>
-        <li>
-          <Link
-            activeClass="active"
-            to="refuteUISReponse"
-            spy={true}
-            smooth={true}
-            duration={500}
-            className={navStyle}
-          >
-            Refute UIS Response
-          </Link>
-        </li>
-        <li>
-          <Link
-            activeClass="active"
-            to="UIUCSystem"
-            spy={true}
-            smooth={true}
-            duration={500}
-            className={navStyle}
-          >
-            UIUC System
-          </Link>
-        </li>
-        <li>
-          <Link
-            activeClass="active"
-            to="divestmentHistory"
-            spy={true}
-            smooth={true}
-            duration={500}
-            className={navStyle}
-          >
-            Divestment History
-          </Link>
-        </li>
+        {sourceText.map(
+          (dataKey: { path: string; text: string }, i: number) => {
+            return (
+              <li key={i}>
+                <Link
+                  activeClass="active"
+                  to={dataKey.path}
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                  className={navStyle}
+                >
+                  {dataKey.text}
+                </Link>
+              </li>
+            )
+          },
+        )}
       </ul>
     </nav>
   )
