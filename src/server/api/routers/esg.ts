7e5a506929f37fbabc4b4ccf0e600e00a6cac2d8
daniel-moon32 <dm32@illinois.edu>
2023-03-02 15:03:31 -0600

    Add completed code with bad narrowing

--- src/server/api/routers/esg.ts
+++ src/server/api/routers/esg.ts
@@ -1,11 +1,19 @@
-import type { ESG, Company, ESGIndex } from '@prisma/client'
+import type { ESG} from '@prisma/client';
+import type { Company, ESGIndex } from '@prisma/client'
 import { z } from 'zod'
 
 import type { Context } from '../trpc'
 import { createTRPCRouter, publicProcedure } from '../trpc'
 
+interface errorMessage {
+  error?: string
+  message?: string
+}
+
+
+
 const MAX_API_CALLS = 50
-const consumeExternalApi = async <T>(url: string): Promise<T> => {
+const consumeExternalApi = async <T>(url: string): Promise<T | errorMessage> => {
   const res = await fetch(url, { method: 'GET' })
   return (await res.json()) as T
 }
@@ -72,49 +80,67 @@ export const esgRouter = createTRPCRouter({
       },
     })
 
-    const company_tickers: Array<string> = []
-    companies.forEach((company: { ticker: string; id: string }) => {
-      if (company.ticker !== 'NO_TICKER_FOUND')
-        company_tickers.push(company.ticker)
-    })
-
-    const url = `https://esgapiservice.com/api/authorization/search?q=${company_tickers
-      .slice(0, MAX_API_CALLS)
-      .join()}&token=${process.env.ESG_API_KEY ?? ''}`
-    const api_res = await consumeExternalApi<ESG[]>(url)
-
-    companies.map(async (company, index) => {
+    companies.map( async (company: { ticker: string; id: string }, index: number) => {
       if (company.ticker !== 'NO_TICKER_FOUND') {
-        const res = await ctx.prisma.eSG.create({
-          data: {
-            environment_grade: api_res[index]?.environment_grade ?? '',
-            environment_score: api_res[index]?.environment_score ?? 0,
-            environment_level: api_res[index]?.environment_level ?? '',
-            company: {
-              connect: {
-                id: company?.id,
+        const url = `https://esgapiservice.com/api/authorization/search?q=${company.ticker}&token=${process.env.ESG_API_KEY ?? ''}`
+      
+        const api_res = await consumeExternalApi<ESG[]>(url)
+        console.log(api_res)
+
+        if ('error' in api_res || 'message' in api_res || !api_res) {
+          console.log(api_res)
+
+          const res = await ctx.prisma.eSGBadTicker.create({
+            data: {
+              company: {
+                connect: {
+                  id: company?.id,
+                }
+              }
+            },
+          })
+
+          if (!res) {
+            return 'Failed to create bad ticker log'
+          }
+
+          return 'No data returned or max calls reached'
+        } 
+        
+          const ESGData = (api_res as ESG[])[0]
+
+          console.log(api_res)
+          const res = await ctx.prisma.eSG.create({
+            data: {
+              environment_grade: ESGData.environment_grade,
+              environment_score: ESGData.environment_score,
+              environment_level: ESGData.environment_level,
+              company: {
+                connect: {
+                  id: company?.id,
+                },
               },
             },
-          },
-        })
+          })
 
-        // Handle error creating ESG
-        if (!res) {
-          await updateESGIndex(
-            ctx,
-            companyIndex as ESGIndex,
-            company as Company,
-          )
-          return 'Error creating ESG'
-        }
+          // Handle error creating ESG
+          if (!res) {
+            await updateESGIndex(
+              ctx,
+              companyIndex as ESGIndex,
+              company as Company,
+            )
+            return 'Error creating ESG'
+          }
 
-        // Update index if last call
-        if (index === MAX_API_CALLS - 1) {
-          await updateESGIndex(
-            ctx,
-            companyIndex as ESGIndex,
-            company as Company,
-          )
+          // Update index if last call
+          if (index === MAX_API_CALLS - 1) {
+            await updateESGIndex(
+              ctx,
+              companyIndex as ESGIndex,
+              company as Company,
+            )
+          }
         }
       }
     })
