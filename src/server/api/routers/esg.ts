7a47baed5c859e4d7c81308e0ce3c0b287da3510
daniel-moon32 <dm32@illinois.edu>
2023-03-02 15:41:24 -0600

    Fix typing

--- src/server/api/routers/esg.ts
+++ src/server/api/routers/esg.ts
@@ -1,4 +1,4 @@
-import type { ESG} from '@prisma/client';
+import type { ESG } from '@prisma/client'
 import type { Company, ESGIndex } from '@prisma/client'
 import { z } from 'zod'
 
@@ -10,12 +10,17 @@ interface errorMessage {
   message?: string
 }
 
-
-
 const MAX_API_CALLS = 50
-const consumeExternalApi = async <T>(url: string): Promise<T | errorMessage> => {
+const consumeExternalApi = async <T>(
+  url: string,
+): Promise<T | errorMessage> => {
   const res = await fetch(url, { method: 'GET' })
   return (await res.json()) as T
+}
+
+// Typeguard for error messages
+const isErrorMessage = (res: errorMessage | ESG[]): res is errorMessage => {
+  return 'error' in res || 'message' in res
 }
 
 const updateESGIndex = async (
@@ -80,70 +85,67 @@ export const esgRouter = createTRPCRouter({
       },
     })
 
-    companies.map( async (company: { ticker: string; id: string }, index: number) => {
-      if (company.ticker !== 'NO_TICKER_FOUND') {
-        const url = `https://esgapiservice.com/api/authorization/search?q=${company.ticker}&token=${process.env.ESG_API_KEY ?? ''}`
-      
-        const api_res = await consumeExternalApi<ESG[]>(url)
-        console.log(api_res)
+    companies.map(
+      async (company: { ticker: string; id: string }, index: number) => {
+        if (company.ticker !== 'NO_TICKER_FOUND') {
+          const url = `https://esgapiservice.com/api/authorization/search?q=${
+            company.ticker
+          }&token=${process.env.ESG_API_KEY ?? ''}`
+          const api_res = await consumeExternalApi<ESG[]>(url)
 
-        if ('error' in api_res || 'message' in api_res || !api_res) {
-          console.log(api_res)
-
-          const res = await ctx.prisma.eSGBadTicker.create({
-            data: {
-              company: {
-                connect: {
-                  id: company?.id,
-                }
-              }
-            },
-          })
-
-          if (!res) {
-            return 'Failed to create bad ticker log'
-          }
-
-          return 'No data returned or max calls reached'
-        } 
-        
-          const ESGData = (api_res as ESG[])[0]
-
-          console.log(api_res)
-          const res = await ctx.prisma.eSG.create({
-            data: {
-              environment_grade: ESGData.environment_grade,
-              environment_score: ESGData.environment_score,
-              environment_level: ESGData.environment_level,
-              company: {
-                connect: {
-                  id: company?.id,
+          if (isErrorMessage(api_res) || !api_res[0]) {
+            const res = await ctx.prisma.eSGBadTicker.create({
+              data: {
+                company: {
+                  connect: {
+                    id: company?.id,
+                  },
                 },
               },
-            },
-          })
+            })
 
-          // Handle error creating ESG
-          if (!res) {
-            await updateESGIndex(
-              ctx,
-              companyIndex as ESGIndex,
-              company as Company,
-            )
-            return 'Error creating ESG'
-          }
+            if (!res) {
+              return 'Failed to create bad ticker log'
+            }
 
-          // Update index if last call
-          if (index === MAX_API_CALLS - 1) {
-            await updateESGIndex(
-              ctx,
-              companyIndex as ESGIndex,
-              company as Company,
-            )
+            return 'No data returned or max calls reached'
+          } else {
+            const ESGData = api_res[0]
+            const res = await ctx.prisma.eSG.create({
+              data: {
+                environment_grade: ESGData.environment_grade,
+                environment_score: ESGData.environment_score,
+                environment_level: ESGData.environment_level,
+                company: {
+                  connect: {
+                    id: company?.id,
+                  },
+                },
+              },
+            })
+
+            // Handle error creating ESG
+            if (!res) {
+              await updateESGIndex(
+                ctx,
+                companyIndex as ESGIndex,
+                company as Company,
+              )
+              return 'Error creating ESG'
+            }
+
+            // Update index if last call
+            if (index === MAX_API_CALLS) {
+              await updateESGIndex(
+                ctx,
+                companyIndex as ESGIndex,
+                company as Company,
+              )
+            }
           }
         }
-      }
-    })
+      },
+    )
 
     return 'Success'
   }),
