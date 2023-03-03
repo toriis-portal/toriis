5580aee60a324ddd24b481d98c2f8c106d54d35e
Alex Masgras <masgras2@illinois.edu>
2023-03-02 21:50:45 -0600

    Start to build raw aggregated investment query

--- src/server/api/routers/investment.ts
+++ src/server/api/routers/investment.ts
@@ -1,3 +1,4 @@
+import { Prisma } from '@prisma/client'
 import { z } from 'zod'
 
 import { createTRPCRouter, publicProcedure } from '../trpc'
@@ -13,12 +14,45 @@ export const investmentRouter = createTRPCRouter({
     .query(async ({ input, ctx }) => {
       const limit = input.limit ?? 10
       const { cursor } = input
-      const items = await ctx.prisma.investment.findMany({
+
+      const xprisma = ctx.prisma.$extends({
+        result: {
+          investment: {
+            total_cost: {
+              // the dependencies
+              needs: { quantity: true, costVal: true },
+              compute(investment) {
+                // the computation logic
+                return investment.quantity * investment.costVal
+              },
+            },
+          },
+        },
+      })
+
+      //temporary, can't get all the needed data
+      const items = await ctx.prisma.company.findMany({
         take: limit + 1, // get an extra item at the end which we'll use as next cursor
         cursor: cursor ? { id: cursor } : undefined,
+        select: {
+          id: true,
+          name: true,
+          sector: true,
+          industry: true,
+        },
       })
+
+      /*const items = await xprisma.company.aggregateRaw({
+        pipeline: [
+          { $limit: limit },
+          { $skip: 1 },
+          { $addFields: { asset_sum: { $sum: "$investment.total_cost" }}},
+          { $project: { _id: 1, name: 1, sector: 1, industry: 1, asset_sum: 1}}
+        ]
+    })*/
+
       let nextCursor: typeof cursor | undefined = undefined
-      if (items.length > limit) {
+      if (items.length && items.length > limit) {
         const nextItem = items.pop()
         nextCursor = nextItem?.id
       }
