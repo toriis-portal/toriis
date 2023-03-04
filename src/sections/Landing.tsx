294410be84d23b44b6dc3c49663defbf8123d725
eeshabarua <eesha.barua@gmail.com>
2023-03-04 13:31:08 -0600

    Finish Styling

--- src/sections/Landing.tsx
+++ src/sections/Landing.tsx
@@ -5,16 +5,22 @@ import dynamic from 'next/dynamic'
 
 import { api } from '../utils/api'
 import data from '../info/home.json'
+import { PrimaryButton } from '../components'
 
 const Chart = dynamic(() => import('react-apexcharts'), { ssr: false })
 
 const Landing: FC = () => {
-  // const hello = api.example.hello.useQuery({ text: 'Toriis' })
-  const source = api.example.countByInvestment.useQuery()
+  const source = api.example.countByInvestment.useQuery(undefined, {
+    refetchOnWindowFocus: false,
+  })
   const firstLetter = clsx(
     'text-4xl first-letter:font-black first-letter:text-[#FFA902] font-medium',
   )
   const landingHeader = clsx('text-4xl font-medium')
+  const textData: string[] = data.landingText.listItems
+  const landingTextStyle = clsx(
+    'font-semibold underline decoration-[#FFA902] decoration-2 underline-offset-4',
+  )
 
   if (!source.data) return <text> Loading...</text>
 
@@ -33,17 +39,31 @@ const Landing: FC = () => {
     return obj
   })
 
-  const options = { labels: labels }
+  const options = {
+    labels: labels,
+    legend: { show: false },
+    colors: ['#FFA902', '#E6F0FA', '#FF6112', '#17292E', '#0F81E8'],
+    dataLabels: {
+      enabled: true,
+      dropShadow: {
+        enabled: false,
+      },
+    },
+    plotOptions: {
+      pie: {
+        donut: {
+          size: '45%',
+        },
+      },
+    },
+  }
   const series = features
 
   console.log(labels)
   return (
     <>
       <div className="min-h-screen">
-        <div className="donut">
-          <Chart options={options} series={series} type="donut" width="800" />
-        </div>
-        <div className="flex place-content-center space-x-8 pt-16">
+        <div className="flex place-content-center space-x-4 pt-16">
           <text className={firstLetter}>Transparent</text>
           <text className={landingHeader}>and</text>
           <text className={firstLetter}>Open</text>
@@ -52,11 +72,39 @@ const Landing: FC = () => {
           <text className={firstLetter}>Institutional</text>
           <text className={firstLetter}>Investments</text>
         </div>
-        <div className="mx-14 flex justify-center gap-4">
-          <text className="w-1/3 place-content-center">place chart here</text>
+        <div className="mx-14 flex justify-center py-10">
+          <div className="donut place-content-center px-14 pt-6">
+            <Chart
+              options={options}
+              series={series}
+              type="donut"
+              width="400"
+              height="400"
+            />
+          </div>
           <div>
-            <text className="w-2/3">{data.landingTextOne}</text>
-            <text className="w-2/3">{data.landingTextTwo}</text>
+            <p className="pr-14">
+              <text>{textData[0]}</text>
+              <text className={landingTextStyle}>{textData[1]}</text>
+              <text>{textData[2]}</text>
+              <text className={landingTextStyle}>{textData[3]}</text>
+            </p>
+            <br></br>
+            <p className="pr-14">
+              <text>{textData[4]}</text>
+              <text className={landingTextStyle}>{textData[5]}</text>
+            </p>
+            <br></br>
+            <div className="flex gap-12 pt-4">
+              <PrimaryButton
+                text={textData[6] ? textData[6] : 'Loading'}
+                link=""
+              />
+              <PrimaryButton
+                text={textData[7] ? textData[7] : 'Loading'}
+                link=""
+              />
+            </div>
           </div>
         </div>
       </div>
