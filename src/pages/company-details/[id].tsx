57e8de94de7225208b80e43570a69099bf3642d5
anthony <anthony.cruzmacedo@protonmail.com>
2023-03-04 14:35:57 -0600

    Complete majority of functionality for retrieving historical closing cost data, small changes to fix warning and errors still needed

--- src/pages/company-details/[id].tsx
+++ src/pages/company-details/[id].tsx
@@ -1,12 +1,15 @@
 import type { FC } from 'react'
 import type { GetServerSideProps } from 'next'
-// import yahooFinance from 'yahoo-finance2'
+import yahooFinance from 'yahoo-finance2'
+import type { HistoricalResult } from 'yahoo-finance2/dist/esm/src/modules/historical'
+import { useEffect, useState } from 'react'
 
 import { prisma } from '../../server/db'
 import { getCompanyTicker } from '../../server/api/routers/company'
 
 interface CompanyDetailsProps {
   ticker: string
+  cost: JSON | string
 }
 
 export const getServerSideProps: GetServerSideProps<
@@ -14,17 +17,39 @@ export const getServerSideProps: GetServerSideProps<
 > = async ({ params }) => {
   let companyID: string
   // grab companyID from url params
+  let data: JSON | string
+
   if (params?.id) {
     companyID = params.id as string
+
     // retrieve ticker by companyID
-    const t = await getCompanyTicker({ prisma: prisma, input: companyID })
+    const t = await getCompanyTicker({
+      prisma: prisma,
+      input: companyID,
+    })
 
     // set props for component
     // some companyIDs are valid but do not have a valid ticker, their return value if it exists in DB is NO_TICKER_FOUND
     // for invalid companyIDs, getCompanyTicker will return 'Invalid CompanyID: No Ticker Found'
+    let res: HistoricalResult | string
+    if (t === 'NO_TICKER_FOUND') {
+      data = 'No ticker found, no historical data found'
+      //   data = 'No data found'
+    } else {
+      const query = t
+      const options = {
+        period1: '2022-02-01',
+      }
+      res = await yahooFinance.historical(query, options)
+      //   res = await yahooFinance.search('AAPL')
+      res = JSON.stringify(res)
+      data = JSON.parse(res) as JSON
+    }
+
     return {
       props: {
         ticker: t,
+        cost: data,
       },
     }
   } else {
@@ -32,24 +57,83 @@ export const getServerSideProps: GetServerSideProps<
     return {
       props: {
         ticker: 'Invalid Company ID: NO_TICKER_FOUND',
+        cost: 'data as string',
       },
     }
   }
-  //   const query = 'TSLA'
-  //   const options = { period1: '2021-02-91' }
-  //   const res: HistoricalResult = (await yahooFinance.historical(
-  //     query,
-  //     options,
-  //   )) as HistoricalResult
-  //   console.log(res)
-  // const ret: HistoricalResult = await yahooFinance.historical(query, options) as HistoricalResult
-  // return ret
 }
 
-export const CompanyDetails: FC<CompanyDetailsProps> = ({ ticker }) => {
+interface DateClosePair {
+  date: string
+  close: number
+}
+
+function createData(cost: JSON): DateClosePair[] {
+  const data: JSON = cost
+  let display: DateClosePair[] = []
+
+  if (typeof data !== 'string') {
+    data.map((obj: DateClosePair) => {
+      const day: string = new Date(obj.date).toLocaleString()
+      const closingPrice: number = obj.close
+      const insert: DateClosePair = {
+        date: day,
+        close: closingPrice,
+      }
+
+      display.push(insert)
+      //   return (
+      //     <p key={date.toDateString()}>
+      //       {ticker} Closing cost on {date.toLocaleDateString()}: ${obj.close}
+      //     </p>
+      //   )
+    })
+    const length = display.length
+    if (length > 100) {
+      display = display.slice(length - 100)
+    }
+  }
+  return display
+}
+
+export const CompanyDetails: FC<CompanyDetailsProps> = ({ ticker, cost }) => {
+  //   const display: DateClosePair[] = []
+  const [display, setDisplayState] = useState([])
+
+  //   const data: JSON | string = cost
+  //   const display: DateClosePair[] = []
+
+  //   if (typeof data !== 'string') {
+  //     data.map((obj: DateClosePair) => {
+  //       const day: string = new Date(obj.date).toLocaleString()
+  //       const closingPrice: number = obj.close
+  //       const insert: DateClosePair = {
+  //         date: day,
+  //         close: closingPrice,
+  //       }
+
+  //       display.push(insert)
+  //       //   return (
+  //       //     <p key={date.toDateString()}>
+  //       //       {ticker} Closing cost on {date.toLocaleDateString()}: ${obj.close}
+  //       //     </p>
+  //       //   )
+  //     })
+  //   }
+
+  useEffect(() => {
+    if (typeof cost !== 'string') setDisplayState(createData(cost))
+  }, [cost, display])
+
   return (
     <>
-      <div>
+      <div className="flex flex-col">
+        {typeof cost !== 'string' &&
+          display.map(({ date, close }) => (
+            <p key={ticker + date}>
+              {ticker} closing cost on {date}: ${close.toFixed(2)}
+            </p>
+          ))}
         <p>{ticker}</p>
       </div>
     </>
