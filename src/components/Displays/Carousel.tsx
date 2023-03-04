75170b718394aa7eefab2ab8f77f465c33edf607
Liza George <liza@chiramattel.com>
2023-03-04 14:01:02 -0600

    Add dots to show current slide

--- src/components/Displays/Carousel.tsx
+++ src/components/Displays/Carousel.tsx
@@ -14,28 +14,49 @@ interface CarouselProps {
 
 interface CarouselChildProps {
   childData: CarouselChildData
-  // width: string
+}
+
+interface CircleDotProps {
+  active: boolean
 }
 
 const CarouselChild: FC<CarouselChildProps> = ({ childData }) => {
   return (
-    <p
-      // className="flex inline-flex w-1/2 break-normal text-black"
-      className="inline-flex"
-      style={{ width: '100%' }}
-    >
+    <p className="inline-flex" style={{ width: '100%' }}>
       {childData.text}
     </p>
+  )
+}
+
+const CarouselDot: FC<CircleDotProps> = ({ active }) => {
+  return (
+    <div
+      className={`h-3.5 w-3.5 rounded-full ${
+        active ? 'bg-pumpkin' : 'bg-lightGray'
+      }`}
+    ></div>
   )
 }
 
 const Carousel: FC<CarouselProps> = ({ carouselChildrenData }) => {
   const [activeIndex, setActiveIndex] = useState(0)
 
+  // const initDotsStatuses: boolean[] = new Array(carouselChildrenData.length).fill(false)
+  const initDotsStatuses: boolean[] = []
+  for (let i = 0; i < carouselChildrenData.length; i++) {
+    initDotsStatuses.push(false)
+  }
+  initDotsStatuses[0] = true
+  const [dotsStatuses, setDotsStatuses] = useState(initDotsStatuses)
+
   const updateIndex = (newIndex: number) => {
+    initDotsStatuses[0] = false
     if (newIndex >= 0 && newIndex < carouselChildrenData.length) {
       setActiveIndex(newIndex)
     }
+    const newStatus = [...initDotsStatuses]
+    newStatus[newIndex] = true
+    setDotsStatuses(newStatus)
   }
 
   return (
@@ -56,9 +77,7 @@ const Carousel: FC<CarouselProps> = ({ carouselChildrenData }) => {
           }}
         >
           {carouselChildrenData.map((childData, index) => {
-            return (
-              <CarouselChild childData={childData} key={index}></CarouselChild>
-            )
+            return <CarouselChild childData={childData} key={index} />
           })}
         </div>
       </div>
@@ -74,6 +93,11 @@ const Carousel: FC<CarouselProps> = ({ carouselChildrenData }) => {
             }`}
           />
         </button>
+        <div className="flex flex-row gap-3">
+          {dotsStatuses.map((status: boolean, index) => {
+            return <CarouselDot active={status} key={index} />
+          })}
+        </div>
         <button
           onClick={() => updateIndex(activeIndex + 1)}
           disabled={activeIndex == carouselChildrenData.length - 1}
