f5bfd10502d2288c645c2ca51fb510c0b44e7b58
Alex Masgras <111911351+masgras2@users.noreply.github.com>
2023-03-05 14:42:08 -0600

    Create investment router with getInvestments procedure (#31)
    * Create getCompanies procedure that includes asset sum

--- src/server/api/routers/company.ts
+++ src/server/api/routers/company.ts
@@ -1,6 +1,59 @@
+import { z } from 'zod'
+
 import { createTRPCRouter, publicProcedure } from '../trpc'
 
 export const companyRouter = createTRPCRouter({
+  getInvestments: publicProcedure
+    .input(
+      z.object({
+        limit: z.number().min(1).max(100).nullish(),
+        cursor: z.string().nullish(),
+      }),
+    )
+    .query(async ({ input, ctx }) => {
+      const limit = input.limit ?? 9
+      const { cursor } = input
+
+      const items = await ctx.prisma.company.findMany({
+        take: limit + 1,
+        cursor: cursor ? { id: cursor } : undefined,
+        select: {
+          id: true,
+          name: true,
+          sector: true,
+          industry: true,
+          investment: {
+            select: {
+              marketVal: true,
+              quantity: true,
+            },
+          },
+          ESG: {
+            select: {
+              environment_grade: true,
+            },
+          },
+        },
+      })
+
+      items.forEach((item) => {
+        let asset_sum = 0
+        item.investment.forEach((iv) => {
+          asset_sum += iv.marketVal * iv.quantity
+        })
+        Object.assign(item, { asset_sum: asset_sum })
+      })
+
+      let nextCursor: typeof cursor | undefined = undefined
+      if (items.length > limit) {
+        const nextItem = items.pop()
+        nextCursor = nextItem?.id
+      }
+      return {
+        items,
+        nextCursor,
+      }
+    }),
   countByInvestment: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.company.groupBy({
       by: ['sector'],
