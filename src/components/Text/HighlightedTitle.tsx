8b471768ea7ab3152dd9075a01380d2674f83271
Xiaofan Li <xl121@illinois.edu>
2023-03-09 18:17:20 -0500

    change font size and font for medium

--- src/components/Text/HighlightedTitle.tsx
+++ src/components/Text/HighlightedTitle.tsx
@@ -18,14 +18,14 @@ const HighlightedTitle: FC<TitleProps> = ({ title, size, color }) => {
             'relative z-0',
             `bg-${color}`,
             { 'right-[.6rem] -bottom-[1.7rem] h-2 w-11/12': size == 'medium' },
-            { 'right-[1.4rem] -bottom-[2.1rem] h-3.5': size == 'large' },
+            { 'right-[1.4rem] -bottom-[2rem] h-3.5': size == 'large' },
           )}
         />
         <p
           className={clsx(
-            'relative z-10 font-klima text-[32px]',
-            { 'font-bold': size == 'large' },
-            { 'font-medium': size == 'medium' },
+            'relative z-10',
+            { 'font-klima text-[32px] font-bold': size == 'large' },
+            { 'font-intel text-[28px] font-medium': size == 'medium' },
           )}
         >
           {title}
