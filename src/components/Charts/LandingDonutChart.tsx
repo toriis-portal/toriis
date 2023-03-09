06d7f3fedb992b0e84f937cfcba826e8c0e0b06c
eeshabarua <eesha.barua@gmail.com>
2023-03-09 01:11:59 -0600

    clean chart data

--- src/components/Charts/LandingDonutChart.tsx
+++ src/components/Charts/LandingDonutChart.tsx
@@ -9,38 +9,54 @@ const LandingDonutChart: FC = () => {
   const source = api.company.countBySector.useQuery(undefined, {
     refetchOnWindowFocus: false,
   })
-  if (!source.data) return <p className="h-96 w-96"> Loading...</p>
 
-  interface dataObj {
-    label: string
-    count: number
-  }
+  if (!source.data) return <p className="h-96 w-96"> Loading...</p>
 
   const pairs: dataObj[] = source.data.map((data) => ({
     label: data.sector as string,
     count: data._count.sector,
   }))
 
-  const total = pairs
-    .map((item) => item.count)
-    .reduce((prev, next) => prev + next)
+  interface dataObj {
+    label: string
+    count: number
+  }
 
-  pairs.sort()
+  // populate the OTHERS item
+  let newCount = pairs
+    .filter((item) => item.count < 23)
+    .reduce((prev, curr) => prev + curr.count, 0)
+
+  newCount += pairs.find((item) => item.label === 'NONE')?.count ?? 0
+
+  pairs.push({ label: 'OTHERS', count: newCount })
+
+  // remove item helper function
+  function removeItem(value: dataObj) {
+    const index = pairs.indexOf(value)
+    if (index > -1) {
+      pairs.splice(index, 1)
+    }
+    return pairs
+  }
+
+  // remove items below threshold 5%
+  function cleanData(pairs: dataObj[]) {
+    pairs.map((item) => {
+      item.count < 23 ? removeItem(item) : console.log('Fine')
+    })
+    pairs.map((item) => {
+      item.label == 'NONE' ? removeItem(item) : console.log('Fine')
+    })
+  }
+
+  cleanData(pairs)
 
   console.log(pairs)
 
-  // @TODO
-  // populate new item
-  // extract
-  // pairs.map((item) =>
-  //   item.count / total < 0.05 ? console.log(item) : console.log('Not'),
-  // )
+  const labels: string[] = pairs.map((dataKey) => dataKey.label)
 
-  const labels: string[] = source.data.map(
-    (dataKey) => dataKey.sector as string,
-  )
-
-  const counts: number[] = source.data.map((dataKey) => dataKey._count.sector)
+  const counts: number[] = pairs.map((dataKey) => dataKey.count)
 
   const options = {
     labels: labels,
