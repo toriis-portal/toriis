d1daa9554e9a262716dff7bc95c347acfae0afd4
Zora Zhang <zorazrr@gmail.com>
2023-03-20 08:35:54 -0500

    Small styling adjustments

--- src/components/Nav/PrimaryNavBar.tsx
+++ src/components/Nav/PrimaryNavBar.tsx
@@ -9,21 +9,23 @@ import toriisimg from '../../../public/toriis.png'
 
 const PrimaryNavBar: FC = () => {
   return (
-    <Navbar fluid={true} rounded={true} className="px-4 sm:py-0.5 sm:px-14">
-      <Link href="/home">
-        <Image
-          src={toriisimg as HTMLImageElement}
-          className="mr-3 h-14 w-auto sm:h-20"
-          alt="Toriis Logo"
-        />
-      </Link>
-      <Navbar.Toggle />
-      <Navbar.Collapse>
-        <NavButton title="Home" link="/home" />
-        <NavButton title="Fossil Fuel" link="/fossil-fuel" />
-        <NavButton title="Learn about Investments" link="/investments" />
-      </Navbar.Collapse>
-    </Navbar>
+    <div className="px-4 sm:px-6">
+      <Navbar fluid={true} rounded={true}>
+        <Link href="/home">
+          <Image
+            src={toriisimg as HTMLImageElement}
+            className="mr-3 h-14 w-auto sm:h-20"
+            alt="Toriis Logo"
+          />
+        </Link>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <NavButton title="Home" link="/home" />
+          <NavButton title="Fossil Fuel" link="/fossil-fuel" />
+          <NavButton title="Learn about Investments" link="/investments" />
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
   )
 }
 
