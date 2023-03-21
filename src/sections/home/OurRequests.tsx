2931f5a68d28f86b648f90c315c3c3d0b72e21ce
21xiaofanli <91353008+21xiaofanli@users.noreply.github.com>
2023-03-20 21:57:48 -0500

    Add sizing and color to highlightedtitle (#54)
    * Add sizing and color to highlightedtitle
    
    * change font size and font for medium
    
    * change the titles on homepage to large and unbolded large title
    
    * Delete unnecessary comments and change paramter of color
    
    * switch prop parameters
    
    * Add font-medium
    
    * Re-add highlighted title styling on home page
    
    * Restyled Divestment History

--- src/sections/home/OurRequests.tsx
+++ src/sections/home/OurRequests.tsx
@@ -6,7 +6,7 @@ import type { OurRequestsEntry } from '../../types'
 const OurRequest: FC<{ entries: OurRequestsEntry[] }> = ({ entries }) => {
   return (
     <div className="bg-lightBlue p-12">
-      <HighlightedTitle title="Our Requests" />
+      <HighlightedTitle title="Our Requests" size="large" color="clementine" />
       {entries.map((entry, index) => (
         <div key={index}>
           <RequestAccordion content={entry} />
