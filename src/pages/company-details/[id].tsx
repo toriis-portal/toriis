d30da00e076266d6d72830cafe8f0bf894cfb2ff
acruzm3 <acruzm3@illinois.edu>
2023-03-23 15:18:07 -0500

    Pass priceData to front end, some loading errors with cleaning

--- src/pages/company-details/[id].tsx
+++ src/pages/company-details/[id].tsx
@@ -1,91 +1,92 @@
+import { useRouter } from 'next/router'
 import type { FC } from 'react'
-import type { GetServerSideProps } from 'next'
-import yahooFinance from 'yahoo-finance2'
-import type { HistoricalResult } from 'yahoo-finance2/dist/esm/src/modules/historical'
-import { useEffect, useState } from 'react'
+// import type { GetServerSideProps } from 'next'
+// import yahooFinance from 'yahoo-finance2'
+// import type { HistoricalResult } from 'yahoo-finance2/dist/esm/src/modules/historical'
+// import { useEffect, useState } from 'react'
 
-import { prisma } from '../../server/db'
-import { getCompanyTicker } from '../../server/api/routers/company'
+// import { prisma } from '../../server/db'
+// import { getCompanyData } from '../../server/api/routers/company'
+
+import { api } from '../../utils/api'
 
 interface CompanyDetailsProps {
-  ticker: string
-  costData: DateClosePair[] | string
-  valid: boolean
+  id: string
 }
 
-export const getServerSideProps: GetServerSideProps<
-  CompanyDetailsProps
-> = async ({ params }) => {
-  let companyID: string
-  let data: DateClosePair[] | string = 'No historical data found'
-  let val = false // assumes an error will occur, set to true after data validation
+// export const getServerSideProps: GetServerSideProps<
+//   CompanyDetailsProps
+// > = async ({ params }) => {
+//   let companyID: string
+//   let data: DateClosePair[] | string = 'No historical data found'
+//   let val = false // assumes an error will occur, set to true after data validation
 
-  // grab companyID from url params
-  if (params?.id) {
-    // if an ID exists in URL, it should be a company ID
-    companyID = params.id as string
+//   // grab companyID from url params
+//   if (params?.id) {
+//     // if an ID exists in URL, it should be a company ID
+//     companyID = params.id as string
 
-    // retrieve ticker by companyID
-    // case 1: t = 'NO_TICKER_FOUND' -> companyID is valid & stored in DB but no ticker exists
-    // case 2: t = 'Invalid CompanyID: No Ticker Found' -> companyID is invalid and/or not stored in DB
-    // case 3: t = TICKER -> companyID is valid & stored in DB with an existing ticker
-    const t = await getCompanyTicker({
-      prisma: prisma,
-      input: companyID,
-    })
+//     // retrieve ticker by companyID
+//     // case 1: t = 'NO_TICKER_FOUND' -> companyID is valid & stored in DB but no ticker exists
+//     // case 2: t = 'Invalid CompanyID: No Ticker Found' -> companyID is invalid and/or not stored in DB
+//     // case 3: t = TICKER -> companyID is valid & stored in DB with an existing ticker
+//     const t = await getCompanyTicker({
+//       prisma: prisma,
+//       input: companyID,
+//     })
 
-    let res: HistoricalResult | string
+//     let res: HistoricalResult | string
 
-    // case 0
-    if (t === 'NO_TICKER_FOUND') {
-      data = 'No historical data found: no ticker exists for this company'
+//     // case 0
+//     if (t === 'NO_TICKER_FOUND') {
+//       data = 'No historical data found: no ticker exists for this company'
 
-      // case 1
-    } else if (t === 'Invalid CompanyID: No Ticker Found') {
-      data = 'No historical data found: this company does not exist'
+//       // case 1
+//     } else if (t === 'Invalid CompanyID: No Ticker Found') {
+//       data = 'No historical data found: this company does not exist'
 
-      // case 3
-    } else {
-      // query yahoo-finance2 using valid ticker
-      const query = t
+//       // case 3
+//     } else {
+//       // query yahoo-finance2 using valid ticker
+//       const query = t
 
-      // query data from period1 (past) to today (present)
-      const options = {
-        period1: '2022-02-01',
-      }
+//       // query data from period1 (past) to today (present)
+//       const options = {
+//         period1: '2022-02-01',
+//       }
 
-      // await result, if valid then populate data and return valid
-      await yahooFinance
-        .historical(query, options)
-        .then((ret) => {
-          res = JSON.stringify(ret)
-          data = JSON.parse(res) as DateClosePair[]
-          val = true
-        })
-        .catch(() => {
-          // if invalid, populate data with an error message and leave valid = false
-          data = 'No historical data found'
-        })
-    }
+//       // await result, if valid then populate data and return valid
+//       await yahooFinance
+//         .historical(query, options)
+//         .then((ret) => {
+//           res = JSON.stringify(ret)
+//           data = JSON.parse(res) as DateClosePair[]
+//           val = true
+//         })
+//         .catch(() => {
+//           // if invalid, populate data with an error message and leave valid = false
+//           data = 'No historical data found'
+//         })
+//     }
 
-    return {
-      props: {
-        ticker: t,
-        costData: data,
-        valid: val,
-      },
-    }
-  } else {
-    // unable to retreive id from params, invalid case
-    return {
-      props: {
-        ticker: 'Unable to retrieve company details',
-        costData: 'No historical data found',
-        valid: val,
-      },
-    }
-  }
-}
+//     return {
+//       props: {
+//         ticker: t,
+//         costData: data,
+//         valid: val,
+//       },
+//     }
+//   } else {
+//     // unable to retreive id from params, invalid case
+//     return {
+//       props: {
+//         ticker: 'Unable to retrieve company details',
+//         costData: 'No historical data found',
+//         valid: val,
+//       },
+//     }
+//   }
+// }
 
 interface DateClosePair {
   date: string
@@ -95,51 +96,83 @@ interface DateClosePair {
 // given a JSON data object
 function createData(costData: DateClosePair[]): DateClosePair[] {
   const data: DateClosePair[] = costData
-  let display: DateClosePair[] = []
+  console.log(data)
+  // let display: DateClosePair[] = []
 
-  if (typeof data !== 'string' && data) {
-    data.map((obj: DateClosePair) => {
-      const day: string = new Date(obj.date).toLocaleDateString()
-      const closingPrice: number = obj.close
-      const insert: DateClosePair = {
-        date: day,
-        close: closingPrice,
-      }
+  // if (typeof data !== 'string' && data) {
+  //   console.log(data)
+  //   data.map((obj: DateClosePair) => {
+  //     const day: string = new Date(obj.date).toLocaleDateString()
+  //     const closingPrice: number = obj.close
+  //     const insert: DateClosePair = {
+  //       date: day,
+  //       close: closingPrice,
+  //     }
 
-      display.push(insert)
-    })
-    const length = display.length
-    if (length > 100) {
-      display = display.slice(length - 100)
-    }
-    return display.reverse()
-  } else {
-    return []
-  }
+  //     display.push(insert)
+  //   })
+  //   const length = display.length
+  //   if (length > 100) {
+  //     display = display.slice(length - 100)
+  //   }
+  //   return display.reverse()
+  // } else {
+  return []
 }
 
-export const CompanyDetails: FC<CompanyDetailsProps> = ({
-  ticker,
-  costData,
-  valid,
-}) => {
+export const CompanyDetails: FC<CompanyDetailsProps> = () => {
   // set to an empty array first
-  const [display, setDisplay] = useState<DateClosePair[]>([])
+  const uuid: string = (useRouter().query.id as string) ?? ''
 
-  useEffect(() => {
-    // only call if data is a non-empty DataClosePair[]
-    // set our display data accordignly
-    if (valid && typeof costData !== 'string' && costData.length)
-      setDisplay(createData(costData))
-    // otherwise set display to empty
-  }, [costData, valid]) // rerender if any of these values change to avoid hydration errors
+  let enabled = false
+
+  if (uuid != '') {
+    // console.log('empty')
+    enabled = true
+  }
+
+  // console.log(uuid)
+  // const [display, setDisplay] = useState<DateClosePair[]>([])
+
+  const priceData = api.company.getCompanyData.useQuery(
+    { id: uuid },
+    { enabled: enabled },
+  )
+
+  let parsedCompanyData: DateClosePair[]
+  // temporary handler for bad yahoo finance requests (no ticker found)
+  if (priceData.error) {
+    return null
+  } else {
+    const toParse = JSON.stringify(priceData)
+    parsedCompanyData = JSON.parse(toParse) as DateClosePair[]
+    parsedCompanyData = createData(parsedCompanyData)
+  }
+
+  console.log(parsedCompanyData)
+  // api.company.useQuery({ input: uuid })
+  // console.log(priceData)
+  // const dat = api.company.getCompanyData.useQuery(id).data as DateClosePair[]
+
+  // console.log(dat)s
+
+  // useEffect(() => {
+  //   // only call if data is a non-empty DataClosePair[]
+  //   // set our display data accordignly
+  //   if (valid && typeof costData !== 'string' && costData.length)
+  //     setDisplay(createData(costData))
+  //   // otherwise set display to empty
+  // }, [costData, valid]) // rerender if any of these values change to avoid hydration errors
 
   // handles hydration error, if the display is empty then do not render first
   return (
     <>
-      {/* render only one of error message or details, this logic format avoids
+      <div>
+        <p>Hi</p>
+      </div>
+      {/* {/* render only one of error message or details, this logic format avoids
       hydration errors */}
-      {((!display.length || !valid) && typeof costData === 'string' && (
+      {/* {((!display.length || !valid) && typeof costData === 'string' && (
         <div>
           <p>{costData}</p>
         </div>
@@ -162,7 +195,7 @@ export const CompanyDetails: FC<CompanyDetailsProps> = ({
               </tbody>
             </table>
           </div>
-        ))}
+        ))} */}
     </>
   )
 }
--- src/server/api/routers/company.ts
+++ src/server/api/routers/company.ts
@@ -1,48 +1,83 @@
 import { z } from 'zod'
 import type { Company } from '@prisma/client'
-import type { PrismaClient } from '@prisma/client'
+import yahooFinance from 'yahoo-finance2'
+import { TRPCError } from '@trpc/server'
 
 import { createTRPCRouter, publicProcedure } from '../trpc'
 
-export async function getCompanyByID({
-  prisma,
-  input,
-}: {
-  prisma: PrismaClient
-  input: string
-}): Promise<Company | null> {
-  const company: Company | null = await prisma.company
-    .findUnique({
-      where: {
-        id: input,
-      },
-    })
-    .catch(() => {
-      return null
-    })
-  return company
-}
-
-export async function getCompanyTicker({
-  prisma,
-  input,
-}: {
-  prisma: PrismaClient
-  input: string
-}): Promise<string> {
-  const company: Company | null = await getCompanyByID({
-    prisma: prisma,
-    input,
-  })
-
-  if (company && company.ticker) {
-    return company.ticker
-  }
-  return 'Invalid CompanyID: No Ticker Found'
-}
+// interface for data returned by yahoo finance pkg
+// interface DateClosePair {
+//   date: string // date queried
+//   close: number // closing cost on date queried
+// }
 
 export const companyRouter = createTRPCRouter({
-  getTicker: publicProcedure.input(z.string()).query(({ ctx, input }) => {
-    return getCompanyTicker({ prisma: ctx.prisma, input })
-  }),
+  // getTicker: publicProcedure.input(z.string()).query(({ ctx, input }) => {
+  //   return getCompanyTicker({ prisma: ctx.prisma, input })
+  // }),
+  // given a company id, find company in db
+  // then grab ticker,
+  // then query yahoo-finance with ticker
+  getCompanyData: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input }) => {
+      // given a company id, find company in db
+      console.log('INPUT: ' + input.id)
+      const company: Company | null = await ctx.prisma.company.findUnique({
+        where: {
+          id: input.id,
+        },
+      })
+
+      if (!company) {
+        throw new TRPCError({
+          code: 'INTERNAL_SERVER_ERROR',
+          message: 'Error fetching company',
+        })
+      }
+
+      // given the company, grab the ticker as query value
+      const query: string = company.ticker
+
+      if (query === 'NO_TICKER_FOUND') {
+        throw new TRPCError({
+          code: 'BAD_REQUEST',
+          message: 'No Ticker Found',
+        })
+      }
+      const options = {
+        period1: '2022-02-01',
+      }
+
+      // initialize data array
+      // let companyData: DateClosePair[]
+
+      // initialize temp result from yahoo finance
+      // pkg returns type HistoricalResult so we
+      // will parse it into a JSON object then cast it to DateClosePair[]
+      // let res: string
+
+      // given the company ticker, query yahoo finance
+      const ret = yahooFinance.historical(query, options)
+
+      return ret
+
+      // .then((ret) => {
+      //   // while (ret == undefined) {
+      //   //   if (ret != undefined) {
+      //   //     return ret
+      //   //   }
+      //   // }
+      //   console.log(ret)
+      //   return ret
+      //   // res = JSON.stringify(ret)
+      //   // return res
+      //   // companyData = JSON.parse(res) as DateClosePair[]
+      //   // return companyData
+      // })
+      // .catch((error: unknown) => {
+      //   // if invalid, populate data with an error message and leave valid = false
+      //   return error
+      // })
+    }),
 })
