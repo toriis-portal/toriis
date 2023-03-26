a264275c7cecb92cdc94db964cfb207b4e6fa693
Xiaofan Li <xl121@illinois.edu>
2023-03-26 02:02:17 -0500

    Fix styling for Tooltip

--- src/components/Buttons/ToolTipButton.tsx
+++ src/components/Buttons/ToolTipButton.tsx
@@ -1,59 +1,48 @@
 import type { FC } from 'react'
 import clsx from 'clsx'
-import { useState } from 'react'
+import { InformationCircleIcon } from '@heroicons/react/24/outline'
+import 'flowbite'
 interface TitleProps {
-  type: 'sector' | 'industry' | 'net asset sum' | 'environmental grade'
+  title?: string
+  details?: string
+  child?: React.ReactNode
 }
 
-const ToolTip: FC<TitleProps> = ({ type }) => {
-  const [isTooltipVisible, setIsTooltipVisible] = useState(false)
-
-  const handleMouseEnter = () => {
-    setIsTooltipVisible(true)
-  }
-
-  const handleMouseLeave = () => {
-    setIsTooltipVisible(false)
-  }
-
+const ToolTip: FC<TitleProps> = ({ title, details, child }) => {
   return (
     <>
-      <div className="mb-12 ml-[1.4rem] w-fit">
+      <div className="group relative inline-block">
         <button
-          data-tooltip-target="tooltip-top"
-          data-tooltip-placement="bottom"
-          type="button"
-          className="mb-2 rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 md:mb-0"
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-        >
-          {type}
-        </button>
-        <div
-          id="tooltip-bottom"
-          role="tooltip"
           className={clsx(
-            'absolute',
-            'z-10',
-            'inline-block',
-            'px-3',
-            'py-2',
-            'text-sm',
-            'font-medium',
-            'text-white',
-            'bg-gray-900',
-            'rounded-lg',
-            'shadow-sm',
-            'tooltip',
-            'dark:bg-gray-700',
-            {
-              'opacity-0': !isTooltipVisible,
-              'opacity-100': isTooltipVisible,
-            },
+            'bg-primary inline-flex rounded py-2 px-[18px]',
+            'text-base text-black',
           )}
         >
-          Tooltip on bottom
-          <div className="tooltip-arrow" data-popper-arrow></div>
+          <InformationCircleIcon className="w-6" />
+        </button>
+        <div
+          className={clsx(
+            'border-light text-body-color absolute top-14 left-1/2',
+            '-translate-x-1/2 rounded border border-black bg-white',
+            'text-sm font-normal opacity-0 group-hover:opacity-100',
+            'w-60 p-5 leading-4',
+          )}
+        >
+          <span
+            className={clsx(
+              'absolute -top-4 left-1/2 bottom-7 -z-10 h-8 w-8',
+              '-translate-x-1/2 rotate-45 rounded-sm border-t border-l',
+              'border-black bg-white',
+            )}
+          />
+          {child ? (
+            child
+          ) : (
+            <>
+              <div className=" font-semibold">{title}:</div>
+              {details}
+            </>
+          )}
         </div>
       </div>
     </>
