bebf7e2672f817cb0c83b4c0881944125b54ca28
eeshabarua <eesha.barua@gmail.com>
2023-03-30 17:56:02 -0500

    Works

--- src/components/Buttons/BackButton.tsx
+++ src/components/Buttons/BackButton.tsx
@@ -1,20 +1,30 @@
 import type { FC } from 'react'
-import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { ArrowBack } from '@material-ui/icons'
 import clsx from 'clsx'
 
-const PrimaryButton: FC = () => {
+const BackButton: FC = () => {
+  const router = useRouter()
+
+  const handleClick = () => {
+    router.back()
+  }
+
   return (
-    <div>
-      <Link href={}>
-        <div>
-          <button>
-            <ArrowBack className="inline h-9 w-5 stroke-current stroke-1" />
-          </button>
-        </div>
-      </Link>
-    </div>
+    <button
+      onClick={handleClick}
+      className={clsx(
+        clsx(
+          'justify-centerfont-klima flex w-20 place-content-center items-center text-[18px]',
+          'shadow-gray shadow-lg',
+          'rounded-3xl border-[1px] border-black py-1 px-3',
+        ),
+      )}
+    >
+      <ArrowBack style={{ fontSize: 20 }} />
+      <p className="font-[400]"> Back</p>
+    </button>
   )
 }
 
-export default PrimaryButton
+export default BackButton
--- src/pages/fossil-fuel/index.tsx
+++ src/pages/fossil-fuel/index.tsx
@@ -1,7 +1,13 @@
 import type { FC } from 'react'
 
+import BackButton from '../../components/Buttons/BackButton'
+
 const Home: FC = () => {
-  return <div>Fossil Fuel Page</div>
+  return (
+    <>
+      <div>Fossil Fuel Page</div>
+    </>
+  )
 }
 
 export default Home
