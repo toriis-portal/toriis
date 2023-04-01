300af76edbd5afcea507eb8bfe785630baec387c
Zora Zhang <zorazrr@gmail.com>
2023-03-31 22:18:39 -0500

    Joos demanded sizing changes

--- src/components/Buttons/AuthButton.tsx
+++ src/components/Buttons/AuthButton.tsx
@@ -10,7 +10,7 @@ const AuthButton: FC = () => {
   const [open, setOpen] = useState(false)
 
   const buttonStyle = clsx(
-    'bg-white border border-solid border-2 border-darkTeal text-darkTeal w-full rounded-lg py-3 my-2',
+    'bg-white border border-solid border-2 border-darkTeal text-darkTeal w-full rounded-lg py-3 px-1 my-2',
   )
   const buttonStyleInverse = clsx(
     'bg-darkTeal text-white w-full rounded-lg py-3 mt-6',
@@ -20,10 +20,10 @@ const AuthButton: FC = () => {
     <React.Fragment>
       {session ? (
         <button
-          className="h-full rounded-full bg-black p-2 text-center text-sm font-medium text-white"
+          className="flex aspect-square items-center justify-center rounded-full bg-black p-1.5"
           onClick={() => setOpen(!open)}
         >
-          <UserIcon className="w-[2em] text-white" />
+          <UserIcon className="w-[1.6em] text-white" />
         </button>
       ) : (
         <button
@@ -35,10 +35,10 @@ const AuthButton: FC = () => {
       )}
       <div
         className={clsx(
-          'rounded-lg border border-black bg-white px-2 py-6 lg:px-8',
+          'rounded-lg border border-black bg-white py-6 px-2 lg:px-2',
           'font-inter text-base font-normal text-black',
           'absolute right-8 top-[5.5rem] inline-block',
-          'z-[100] w-[42%] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.3)] transition-opacity duration-200',
+          'z-[100] w-[25%] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.3)] transition-opacity duration-200',
           { 'invisible opacity-0': !open, 'visible opacity-100': open },
         )}
       >
