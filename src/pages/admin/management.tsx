c538744c2f2a54558938dda2edc354be1e79b4e4
Liza George <liza@chiramattel.com>
2023-04-06 18:09:21 -0500

    Test call deleteManyUsers in admin management, need to test with whitelist users

--- src/pages/admin/management.tsx
+++ src/pages/admin/management.tsx
@@ -3,9 +3,19 @@ import { useRouter } from 'next/router'
 import type { FC } from 'react'
 import { useEffect } from 'react'
 
+import { api } from '../../utils/api'
+
 const AdminAdminPage: FC = () => {
   const { data: session, status } = useSession()
   const { push } = useRouter()
+
+  const mutation = api.user.deleteManyUsers.useMutation() // TODO: check about options here
+
+  const handleDeleteUsers = () => {
+    console.log('deleting users')
+    const ids = ['642f50d01c844879937b9c69', '642f50e81c844879937b9c70'] // = ['lizatest1', 'lizatest2']
+    mutation.mutate({ ids })
+  }
 
   useEffect(() => {
     if (status === 'unauthenticated') {
@@ -13,7 +23,23 @@ const AdminAdminPage: FC = () => {
     }
   }, [push, status])
 
-  return <div>{session && <h1>Administration Management</h1>}</div>
+  return (
+    session && (
+      <div>
+        <h1>Administration Management</h1>
+
+        <button onClick={handleDeleteUsers} disabled={mutation.isLoading}>
+          boom boom
+        </button>
+
+        {mutation.error ? (
+          <p>Something went wrong! {mutation.error.message}</p>
+        ) : (
+          <p>Successful deletion</p>
+        )}
+      </div>
+    )
+  )
 }
 
 export default AdminAdminPage
