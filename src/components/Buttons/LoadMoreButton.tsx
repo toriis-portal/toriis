11d0c1fea498c029e09725dd124cc3b54533421b
Zora Zhang <zorazrr@gmail.com>
2023-04-08 13:46:05 -0500

    Adjust button

--- src/components/Buttons/LoadMoreButton.tsx
+++ src/components/Buttons/LoadMoreButton.tsx
@@ -10,12 +10,11 @@ const LoadMoreButton: FC<LoadMoreButtonProps> = ({ onClick, disabled }) => {
   return (
     <button
       className={clsx(
-        'font-klima text-[28px] font-medium',
+        'font-klima text-lg font-medium',
         'rounded border-2 border-solid border-clementine bg-white',
-        'm-6 px-7 py-3.5',
+        'm-6 px-9 py-2',
         'shadow-[-8px_8px_0px_0px] shadow-clementine',
-        'active:shadow-[-12px_12px_0px_0px] active:shadow-clementine',
-        'hover:shadow-[-12px_12px_0px_0px] hover:shadow-clementine',
+        'hover:shadow-[-5px_5px_0px_0px] hover:shadow-clementine',
         `${disabled ? 'hidden' : ''}`,
       )}
       onClick={onClick}
