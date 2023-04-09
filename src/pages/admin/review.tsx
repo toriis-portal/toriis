4c3a1c74f1cf5a36b5933426aafb09f219142e37
Zora Zhang <zorazrr@gmail.com>
2023-04-08 22:06:59 -0500

    Set up ReviewRequest type

--- src/pages/admin/review.tsx
+++ src/pages/admin/review.tsx
@@ -3,6 +3,8 @@ import { useRouter } from 'next/router'
 import type { FC } from 'react'
 import { useEffect } from 'react'
 
+import { api } from '../../utils/api'
+
 const ReviewPage: FC = () => {
   const { data: session, status } = useSession()
   const { push } = useRouter()
@@ -12,6 +14,10 @@ const ReviewPage: FC = () => {
       void push('/auth/error')
     }
   }, [push, status])
+
+  const { data, isLoading } = api.request.getAllRequests.useQuery()
+
+  console.log(data)
 
   return <div>{session && <h1>Request Management</h1>}</div>
 }
