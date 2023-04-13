d5d44917cc0c20038b7c606dbd76f83360b40b40
Xiaofan Li <xl121@illinois.edu>
2023-04-13 18:06:04 -0500

    Create Search bar and button

--- src/components/Buttons/PrimaryButton.tsx
+++ src/components/Buttons/PrimaryButton.tsx
@@ -6,24 +6,27 @@ import clsx from 'clsx'
 interface PrimaryButtonProps {
   text: string
   link: string
+  hasArrow: boolean
+  className?: string
 }
 
-const PrimaryButton: FC<PrimaryButtonProps> = ({ text, link }) => {
+const PrimaryButton: FC<PrimaryButtonProps> = ({ text, link, hasArrow=true, className }) => {
   return (
     <div>
       <Link href={link}>
         <div>
           <button
             className={clsx(
-              'font-klima text-[18px] font-semibold',
+              className,
+              'font-semibold font-klima text-[18px] ',
               'rounded border-2 border-solid border-cobalt',
-              'rounded bg-lightBlue px-5 py-1 font-semibold',
+              'rounded bg-lightBlue px-5 py-1',
               'shadow-[-8px_8px_0px_0px] shadow-cobalt',
               'hover:shadow-[-5px_5px_0px_0px] hover:shadow-cobalt',
-            )}
+            ) }
           >
             {text}{' '}
-            <ArrowRightIcon className="inline h-9 w-5 stroke-current stroke-1" />
+            {hasArrow && <ArrowRightIcon className="inline h-9 w-5 stroke-current stroke-1" />}
           </button>
         </div>
       </Link>
--- src/pages/admin/management.tsx
+++ src/pages/admin/management.tsx
@@ -3,6 +3,9 @@ import { useRouter } from 'next/router'
 import type { FC } from 'react'
 import { useEffect } from 'react'
 
+import { HighlightedTitle,
+PrimaryButton} from '../../components'
+
 const AdminAdminPage: FC = () => {
   const { data: session, status } = useSession()
   const { push } = useRouter()
@@ -13,7 +16,38 @@ const AdminAdminPage: FC = () => {
     }
   }, [push, status])
 
-  return <div>{session && <h1>Administration Management</h1>}</div>
+  return (
+    <div>
+    {session && 
+      <div>
+        <h1>Administration Management</h1>
+        <div className = "pl-56 pt-10">
+        <HighlightedTitle 
+        title="Invite Administrators"
+        size="medium"
+        color="clementine"/>
+        </div>
+        <div className="flex justify-center">
+            <div className=" w-1/2 flex mr-12 ">
+
+                <input
+                    type="text"
+                    className=" px-4 py-3 w-full bg-white border rounded-md"
+                    placeholder="Invite Gmail"
+                />
+            </div>
+
+                <PrimaryButton
+                text="Invite"
+                link="/fossil-fuel"
+                hasArrow={false}
+                className="px-5 w-48 text-base py-2 font-medium"/>
+        </div>
+
+        
+      </div>}
+    </div>
+    )
 }
 
 export default AdminAdminPage
