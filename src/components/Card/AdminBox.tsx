36b3c3097980d8d8bf7132fd4024111c13675381
Xiaofan Li <xl121@illinois.edu>
2023-04-13 13:48:08 -0500

    Fix padding within the boxes

--- src/components/Card/AdminBox.tsx
+++ src/components/Card/AdminBox.tsx
@@ -2,22 +2,23 @@ import type { FC } from 'react'
 
 interface AdminBoxProps {
   title: string,
-  description: string
+  description: string 
 }
 
 const NetAssetTag: FC<AdminBoxProps> = ({ title, description }) => {
   return (
-    <div className="border border-black rounded-lg w-[25rem] h-[17rem] items-center">
-        <div className = "text-xl font-medium text-center">
-            {title}
-        </div>
-        <br/>
-        <div className = "text-l font-medium text-center">
-            {description}
-        </div>
-      <div className="flex pb-1 text-[28px] font-medium text-cobalt">
+  <div className=" shadow-md border border-black rounded-lg w-[25rem] h-[17rem] py-[20%] text-center">
+    <div className="my-auto flex flex-col justify-around h-full">
+      <div className="text-xl font-medium">
+        {title}
+      </div>
+      <br/>
+      <div className="text-l font-semi">
+        {description}
       </div>
     </div>
+  </div>
+
   )
 }
 
--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -2,6 +2,7 @@ import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
 import type { FC } from 'react'
 import { useEffect } from 'react'
+import Link from 'next/link'
 
 import {AdminBox} from '../../components/index'
 
@@ -19,21 +20,31 @@ const AdminPage: FC = () => {
 
   return (
     <div>
-      {session && <h1>Administration Controls</h1>
-      }
-      <div className = "flexbox mb-2 items-center">
-        <div>
-        <AdminBox title="Administration Management" description="Invite new administrators, manage current admins, and manage mailing list of current admins who have the ability to approve new admins" />
+      {session && (
+      <div className = "h-screen">
+      <h1>Administration Controls</h1>
+    <div className="h-5/6 flex flex-wrap justify-around items-center px-5">
+        <div className="w-auto">
+        <Link href="/admin/website/data">
+          <AdminBox title="Website Management" description="Manage site wide contents and databases" />
+          </Link>
         </div>
-        <div>
-        <AdminBox title="Administration Management" description="Invite new administrators, manage current admins, and manage mailing list of current admins who have the ability to approve new admins" />
+        
+        <div className="w-auto">
+        <Link href="/admin/management">
+          <AdminBox title="Administration Management" description="Invite new administrators, manage current admins, and manage mailing list of current admins who have the ability to approve new admins" />
+          </Link>
         </div>
-        <div>
-        <AdminBox title="Administration Management" description="Invite new administrators, manage current admins, and manage mailing list of current admins who have the ability to approve new admins" />
+        <div className="w-auto">
+          <Link href = "/admin/request">
+          <AdminBox title="Request Management" description="View and check status of requests" />
+          </Link>
         </div>
-        </div>
-      
+      </div>
     </div>
+    )
+  }
+   </div>
     )
 }
 
