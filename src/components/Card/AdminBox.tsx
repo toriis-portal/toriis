84544692e56571a0973eceebacfeefa5f84c9fa8
Xiaofan Li <xl121@illinois.edu>
2023-04-13 17:07:36 -0500

    Add blue hover on admin box

--- src/components/Card/AdminBox.tsx
+++ src/components/Card/AdminBox.tsx
@@ -7,7 +7,7 @@ interface AdminBoxProps {
 
 const NetAssetTag: FC<AdminBoxProps> = ({ title, description }) => {
   return (
-  <div className=" shadow-md border border-black rounded-lg w-[25rem] h-[17rem] py-[20%] text-center">
+  <div className="border border-black hover:border-2 hover:border-cobalt rounded-lg w-[25rem] h-[17rem] py-[20%] text-center shadow-md ">
     <div className="my-auto flex flex-col justify-around h-full">
       <div className="text-xl font-medium">
         {title}
