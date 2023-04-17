c605dee3912e8a050d5e11167b7effb9cb5baba5
eeshabarua <eesha.barua@gmail.com>
2023-04-17 17:00:10 -0500

    Use Next Script

--- src/components/Displays/ClimateClock/ClimateClock.tsx
+++ src/components/Displays/ClimateClock/ClimateClock.tsx
@@ -1,48 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import Script from 'next/script'
 
 const ClimateClock = () => {
-  const [isMounted, setIsMounted] = useState(false)
-
-  /*
-   * This useEffect checks to see if the Climate Clock widget is truthy (whether or not it has already been rendered)
-   * before loading the script and appending the climate clock widget to the container.
-   */
-  useEffect(() => {
-    if (!isMounted) {
-      // Declare a custom script element and load the Climate Clock script.
-      const script = document.createElement('script')
-      script.src = 'https://climateclock.world/widget-v2.js'
-      script.async = true
-      script.onload = () => {
-        const climateClock = document.createElement('climate-clock')
-        const container = document.getElementById('climate-clock-container')
-        if (container) {
-          container.appendChild(climateClock)
-          setIsMounted(true)
-        } else {
-          console.error(
-            'Could not find element with ID "climate-clock-container"',
-          )
-        }
-      }
-      document.body.appendChild(script)
-    }
-
-    return () => {
-      const climateClock = document.querySelector('climate-clock')
-      // If the component has already been loaded, remove the duplicate.
-      if (climateClock) {
-        climateClock.remove()
-      }
-    }
-  }, [isMounted])
-
   return (
     <>
-      <div
-        id="climate-clock-container"
-        className="mx-auto w-5/6 items-center"
-      ></div>
+      <div id="climate-clock-container" className="mx-auto w-5/6 items-center">
+        <Script src="https://climateclock.world/widget-v2.js" async />
+      </div>
     </>
   )
 }
--- src/pages/fossil-fuel/index.tsx
+++ src/pages/fossil-fuel/index.tsx
@@ -1,10 +1,9 @@
 import type { FC } from 'react'
 
-import { PrimaryNavBar, ToTopButton } from '../../components'
+import { PrimaryNavBar, ToTopButton, ClimateClock } from '../../components'
 import { ContentWrapper } from '../../utils/content'
 import { FinancialCase } from '../../sections'
 import type { CaseEntry } from '../../types'
-import ClimateClock from '../../components/Displays/ClimateClock/ClimateClock'
 
 export const getServerSideProps = async () => {
   const contentClient = new ContentWrapper()
