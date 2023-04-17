2ee6a5fb66daab0102504613a4be2211c3d91790
Liza George <liza@chiramattel.com>
2023-04-17 17:23:31 -0500

    Create and style WebsiteManagementNavBar, forgot to add

---
+++ src/components/Nav/WebsiteManagementNavBar.tsx
@@ -0,0 +1,38 @@
+import type { FC } from 'react'
+import Link from 'next/link'
+
+import { BackButton, AuthButton } from '../../components'
+
+interface NavItem {
+  path: string
+  text: string
+  active: boolean
+}
+
+const WebsiteManagementNavBar: FC<{ navItems: NavItem[] }> = ({ navItems }) => {
+  return (
+    <div className="flex flex-row items-center justify-between border-b-[3px] border-cobalt px-10 py-8">
+      <div className="h-10">
+        <BackButton />
+      </div>
+      <div className="flex flex-row space-x-12">
+        {navItems.map((navItem: NavItem, i: number) => {
+          return (
+            <Link key={i} href={navItem.path}>
+              <p className="text-[22px]">{navItem.text}</p>
+              <div
+                className={`relative right-[1.3rem] z-0 h-2 ${
+                  navItem.active ? 'bg-clementine' : ''
+                }`}
+              />
+            </Link>
+          )
+        })}
+      </div>
+      <div className="flex w-fit flex-col justify-center">
+        <AuthButton />
+      </div>
+    </div>
+  )
+}
+export default WebsiteManagementNavBar
