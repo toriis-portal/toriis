8f571a160a2386f39d1d03b1b524f0138f1cf4f6
Liza George <liza@chiramattel.com>
2023-04-17 16:06:06 -0500

    Start and style main admin nav bar

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -3,6 +3,8 @@ import { useRouter } from 'next/router'
 import type { FC } from 'react'
 import { useEffect } from 'react'
 
+import { HighlightedTitle, BackButton, AuthButton } from '../../components'
+
 const AdminPage: FC = () => {
   const { data: session, status } = useSession()
   const { push } = useRouter()
@@ -15,7 +17,27 @@ const AdminPage: FC = () => {
 
   console.log(session?.user)
 
-  return <div>{session && <h1>Administration Control</h1>}</div>
+  return (
+    session && (
+      <div>
+        <div className="flex flex-row items-center	justify-between px-10">
+          <div className="h-10">
+            <BackButton customLink="/home" customText="Back To Home" />
+          </div>
+          <div className="mt-10">
+            <HighlightedTitle
+              title="Administration Control"
+              size="large"
+              color="clementine"
+            ></HighlightedTitle>
+          </div>
+          <div className="flex w-fit flex-col justify-center">
+            <AuthButton />
+          </div>
+        </div>
+      </div>
+    )
+  )
 }
 
 export default AdminPage
