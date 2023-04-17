6bd3611761432a38ff77656f086573143978ed2f
anthony <anthony.cruzmacedo@protonmail.com>
2023-04-17 17:16:36 -0500

    Delete contentful button component, add straight into content page

--- src/pages/admin/website/content.tsx
+++ src/pages/admin/website/content.tsx
@@ -3,7 +3,8 @@ import { useRouter } from 'next/router'
 import type { FC } from 'react'
 import { useEffect } from 'react'
 
-import ContentfulButton from '../../../components/Buttons/ContentfulButton'
+import { PrimaryButton } from '../../../components'
+import { CONTENTFUL_LINK } from '../../../utils/constants'
 
 const ContentPage: FC = () => {
   const { data: session, status } = useSession()
@@ -22,7 +23,17 @@ const ContentPage: FC = () => {
           <h1>Update Text Content</h1>
           <div className="h-screen w-screen">
             {session && <h1>Website Management</h1>}
-            <ContentfulButton />
+
+            <div className="flex h-full w-full flex-col items-center justify-center gap-8">
+              <h2 className="font-klima font-semibold">
+                Manage site wide content through Contentful
+              </h2>
+              <PrimaryButton
+                text="Go to Contentful"
+                link={CONTENTFUL_LINK}
+                className="flex items-center justify-center gap-2 !px-10 text-center"
+              />
+            </div>
           </div>
         </>
       )}
