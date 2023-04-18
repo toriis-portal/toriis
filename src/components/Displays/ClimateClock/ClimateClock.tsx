1a617b958f3dfea7d046832466026b98c632c4a6
Zora Zhang <zorazrr@gmail.com>
2023-04-17 23:09:02 -0500

    Test if this shows up on deployment

--- src/components/Displays/ClimateClock/ClimateClock.tsx
+++ src/components/Displays/ClimateClock/ClimateClock.tsx
@@ -1,10 +1,24 @@
 import Script from 'next/script'
+import type { DetailedHTMLProps, HTMLAttributes } from 'react'
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace JSX {
+    interface IntrinsicElements {
+      'climate-clock': DetailedHTMLProps<
+        HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >
+    }
+  }
+}
 
 const ClimateClock = () => {
   return (
     <>
       <div id="climate-clock-container" className="mx-auto w-5/6 items-center">
         <Script src="https://climateclock.world/widget-v2.js" async />
+        <climate-clock />
       </div>
     </>
   )
