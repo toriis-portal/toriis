7b3ed4ce571053a3056cad85625c3a1b35f96aae
Liza George <liza@chiramattel.com>
2023-04-20 15:18:47 -0500

    Use cslx for styling

--- src/components/Nav/WebsiteManagementNavBar.tsx
+++ src/components/Nav/WebsiteManagementNavBar.tsx
@@ -1,4 +1,5 @@
 import type { FC } from 'react'
+import { clsx } from 'clsx'
 import Link from 'next/link'
 
 import { BackButton, AuthButton } from '../../components'
@@ -11,19 +12,20 @@ interface NavItem {
 
 const WebsiteManagementNavBar: FC<{ navItems: NavItem[] }> = ({ navItems }) => {
   return (
-    <div className="flex flex-row items-center justify-between border-b-[3px] border-cobalt px-10 py-8">
+    <div className="flex items-center justify-between border-b-[3px] border-cobalt px-10 py-8">
       <div className="h-10">
         <BackButton />
       </div>
-      <div className="flex flex-row space-x-12">
+      <div className="flex space-x-12">
         {navItems.map((navItem: NavItem, i: number) => {
           return (
             <Link key={i} href={navItem.path}>
               <p className="text-[22px] font-medium">{navItem.text}</p>
               <div
-                className={`relative right-[1.3rem] z-0 h-2 ${
-                  navItem.active ? 'bg-clementine' : ''
-                }`}
+                className={clsx(
+                  'relative right-[1.3rem] z-0 h-2',
+                  navItem.active && 'bg-clementine',
+                )}
               />
             </Link>
           )
