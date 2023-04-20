7ad5e2925c1ab6dc050123b03c7c262866fddbcd
Anthony Cruz Macedo <90807954+anthonycruzmacedo@users.noreply.github.com>
2023-04-20 16:57:11 -0500

    Update Text Content (#124)
    * Add clsx (#24)
    
    * Complete Update Text page content w link to contentful, changed primary button to center text which may need review
    
    * Add optional additional styles to primary button in order to use flex in contentful button  without affecting other uses
    
    * Update to fit naming conventions, move button to website/content, add contentful link to utils/contants
    
    * Delete contentful button component, add straight into content page
    
    * Merge divs in content
    
    * Remove h1 place holder in content
    
    ---------
    
    Co-authored-by: Zora Zhang <zorazrr@gmail.com>

--- src/pages/admin/website/content.tsx
+++ src/pages/admin/website/content.tsx
@@ -3,6 +3,9 @@ import { useRouter } from 'next/router'
 import type { FC } from 'react'
 import { useEffect } from 'react'
 
+import { PrimaryButton } from '../../../components'
+import { CONTENTFUL_LINK } from '../../../utils/constants'
+
 const ContentPage: FC = () => {
   const { data: session, status } = useSession()
   const { push } = useRouter()
@@ -13,7 +16,23 @@ const ContentPage: FC = () => {
     }
   }, [push, status])
 
-  return <div>{session && <h1>Update Text Content</h1>}</div>
+  return (
+    <>
+      {session && (
+        <div className="flex h-screen w-screen flex-col items-center justify-center gap-8">
+          <h2 className="font-klima font-semibold">
+            Manage site wide content through Contentful
+          </h2>
+
+          <PrimaryButton
+            text="Go to Contentful"
+            link={CONTENTFUL_LINK as string}
+            className="flex items-center justify-center gap-2 !px-10 text-center"
+          />
+        </div>
+      )}
+    </>
+  )
 }
 
 export default ContentPage
