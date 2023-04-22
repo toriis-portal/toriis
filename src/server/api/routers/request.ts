ecc648e5719be45ac1de9a13f87198bfd684e3c3
Zora Zhang <zorazrr@gmail.com>
2023-04-22 10:58:13 -0500

    Set up procedure

--- src/server/api/routers/request.ts
+++ src/server/api/routers/request.ts
@@ -1,3 +1,7 @@
+import { TRPCError } from '@trpc/server'
+import { z } from 'zod'
+import { RequestStatus } from '@prisma/client'
+
 import { createTRPCRouter, protectedProcedure } from '../trpc'
 
 export const requestRouter = createTRPCRouter({
@@ -5,4 +9,31 @@ export const requestRouter = createTRPCRouter({
     const requests = await ctx.prisma.request.findMany({})
     return requests
   }),
+  updateRequest: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        updateAction: z.enum(['approve', 'reject']),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      if (input.updateAction === 'reject') {
+        const rejectedRequest = await ctx.prisma.request.update({
+          where: {
+            id: input.id,
+          },
+          data: {
+            status: RequestStatus.REJECTED,
+          },
+        })
+        if (!rejectedRequest) {
+          throw new TRPCError({
+            code: 'INTERNAL_SERVER_ERROR',
+            message: 'Failed to reject request',
+          })
+        }
+      } else {
+        // Do something else
+      }
+    }),
 })
