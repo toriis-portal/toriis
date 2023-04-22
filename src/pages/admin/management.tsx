2cfa583ceb3c5955933eab0863abdca2f8b9d7c2
Zora Zhang <zorazrr@gmail.com>
2023-04-22 14:01:22 -0500

    Get rid of div

--- src/pages/admin/management.tsx
+++ src/pages/admin/management.tsx
@@ -60,79 +60,75 @@ const AdminAdminPage: FC = () => {
 
   return (
     session && (
-      <div>
-        <div className="flex flex-col items-center gap-5 first-letter:w-full">
-          <div className="mt-20 flex w-3/4 flex-row justify-between">
-            <div className="-mb-[12px] flex flex-row items-start justify-start gap-1">
-              <HighlightedTitle
-                title="Administrative List"
-                size="medium"
-                color="clementine"
+      <div className="flex flex-col items-center gap-5 first-letter:w-full">
+        <div className="mt-20 flex w-3/4 flex-row justify-between">
+          <div className="-mb-[12px] flex flex-row items-start justify-start gap-1">
+            <HighlightedTitle
+              title="Administrative List"
+              size="medium"
+              color="clementine"
+            />
+            <p className="ml-2 mt-2.5 text-medGray">
+              {`(${data?.length || 0} Results)`}
+            </p>
+          </div>
+          <button className="self-end" onClick={handleEdit}>
+            {edit ? (
+              <Tag
+                title="edit"
+                className="border border-black bg-black font-normal text-white"
               />
-              <p className="ml-2 mt-2.5 text-medGray">
-                {`(${data?.length || 0} Results)`}
-              </p>
-            </div>
-            <button className="self-end" onClick={handleEdit}>
-              {edit ? (
+            ) : (
+              <u>
                 <Tag
                   title="edit"
-                  className="border border-black bg-black font-normal text-white"
+                  className="border border-black bg-lightBlue font-normal text-black"
                 />
-              ) : (
-                <u>
-                  <Tag
-                    title="edit"
-                    className="border border-black bg-lightBlue font-normal text-black"
-                  />
-                </u>
-              )}
-            </button>
-          </div>
-          <AdminListTable
-            className="w-3/4"
-            users={data}
-            editEnabled={edit}
-            onTrash={(currentId) => {
-              setUsersToDelete([...usersToDelete, currentId])
-            }}
-            onUndo={(currentId) => {
-              setUsersToDelete(usersToDelete.filter((id) => id !== currentId))
-            }}
-            onCheck={(currentId) => {
-              setUsersEmailUpdate(
-                !usersEmailUpdate.includes(currentId)
-                  ? [...usersEmailUpdate, currentId]
-                  : usersEmailUpdate.filter((id) => id !== currentId),
-              )
-            }}
-            tempDeleted={usersToDelete}
-            tempChecked={usersEmailUpdate}
-          />
-          <div className="pt-6">
-            {!deleteUsersMutation.isLoading &&
-              !updateEmailsMutation.isLoading &&
-              edit && (
-                <PrimaryButton text="Update" onClick={handleUpdateUsers} />
-              )}
-          </div>
+              </u>
+            )}
+          </button>
+        </div>
+        <AdminListTable
+          className="w-3/4"
+          users={data}
+          editEnabled={edit}
+          onTrash={(currentId) => {
+            setUsersToDelete([...usersToDelete, currentId])
+          }}
+          onUndo={(currentId) => {
+            setUsersToDelete(usersToDelete.filter((id) => id !== currentId))
+          }}
+          onCheck={(currentId) => {
+            setUsersEmailUpdate(
+              !usersEmailUpdate.includes(currentId)
+                ? [...usersEmailUpdate, currentId]
+                : usersEmailUpdate.filter((id) => id !== currentId),
+            )
+          }}
+          tempDeleted={usersToDelete}
+          tempChecked={usersEmailUpdate}
+        />
+        <div className="mb-4">
+          {!deleteUsersMutation.isLoading &&
+            !updateEmailsMutation.isLoading &&
+            edit && <PrimaryButton text="Update" onClick={handleUpdateUsers} />}
+        </div>
 
-          {deleteUsersMutation.error && (
-            <p>Something went wrong! {deleteUsersMutation.error.message}</p>
-          )}
-          {updateEmailsMutation.error && (
-            <p>Something went wrong! {updateEmailsMutation.error.message}</p>
-          )}
-          <div className="mb-20 w-3/4">
-            <div className="-mb-[12px] flex flex-row items-start justify-start">
-              <HighlightedTitle
-                title="Invite Administrators"
-                size="medium"
-                color="clementine"
-              />
-            </div>
-            <InviteAdminBar />
+        {deleteUsersMutation.error && (
+          <p>Something went wrong! {deleteUsersMutation.error.message}</p>
+        )}
+        {updateEmailsMutation.error && (
+          <p>Something went wrong! {updateEmailsMutation.error.message}</p>
+        )}
+        <div className="mb-20 w-3/4">
+          <div className="-mb-[12px] flex flex-row items-start justify-start">
+            <HighlightedTitle
+              title="Invite Administrators"
+              size="medium"
+              color="clementine"
+            />
           </div>
+          <InviteAdminBar />
         </div>
       </div>
     )
