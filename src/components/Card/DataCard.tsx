83262f826a1d796c03bf993f574ad1ca1c5f028e
eeshabarua <61480751+eeshabarua@users.noreply.github.com>
2023-04-25 18:24:44 -0500

    Company Details Text Interpretation (#148)
    * Initial Commit
    
    * Update carbonAccounting and yahooFinance
    
    * Working on Conditional Text Render
    
    * Trying Grab Fuel Data
    
    * Working so far
    
    * Implementation done
    
    * Address Changes
    
    * Delete String Decoder
    
    * Address Changes
    
    * Optimize Merge
    
    * Update Names and Key

--- src/components/Card/DataCard.tsx
+++ src/components/Card/DataCard.tsx
@@ -2,7 +2,7 @@ import type { FC } from 'react'
 
 const DataCard: FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <div className="rounded-xl bg-lightClementine p-4 font-inter text-base leading-snug">
+    <div className="rounded-xl bg-[#F4F4F4] p-4 font-inter text-base leading-snug">
       {children}
     </div>
   )
