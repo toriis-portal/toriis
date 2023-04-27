27e3cabbf779b014fbdec30e17d6d61763bcc75c
anthony <anthony.cruzmacedo@protonmail.com>
2023-04-27 17:37:11 -0500

    Reduce font size in primary nav bar, use hard coded 20px with Joos approval

--- src/components/Buttons/NavButton.tsx
+++ src/components/Buttons/NavButton.tsx
@@ -20,7 +20,7 @@ const NavButton: FC<NavButtonProps> = ({ title, link }) => {
           'border-cobalt': isActive,
           'border-white': !isActive,
         },
-        'header-3 hover:border-b-4 hover:border-cobalt hover:duration-100',
+        'header-3 text-[20px] hover:border-b-4 hover:border-cobalt hover:duration-100',
       )}
     >
       {title}
