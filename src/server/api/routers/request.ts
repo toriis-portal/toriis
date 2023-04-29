d2b067b5c4c87085b8a2059edd79d20c2a725d89
Alex Masgras <111911351+masgras2@users.noreply.github.com>
2023-04-29 11:56:59 -0500

    Review Request Table (#147)
    * Create placeholder table
    
    * table
    
    * Create box with text-length-dependent popup option
    
    * Finish table style
    
    * query, status tag, style, etc etc
    
    * clean console logs and unused import
    
    * Changed spacing to work with admin navbar
    
    * More styling changes
    
    * Change tag logic
    
    * Final stylings and function extraction
    
    * One last rename
    
    ---------
    
    Co-authored-by: Zora Zhang <zorazrr@gmail.com>

--- src/server/api/routers/request.ts
+++ src/server/api/routers/request.ts
@@ -12,10 +12,50 @@ type UpdateQuery = Omit<UpdateType, 'maturityDate' | 'date'> & {
 }
 
 export const requestRouter = createTRPCRouter({
-  getAllRequests: protectedProcedure.query(async ({ ctx }) => {
-    const requests = await ctx.prisma.request.findMany({})
-    return requests
-  }),
+  getRequests: protectedProcedure
+    .input(
+      z.object({
+        limit: z.number().min(1).max(100).nullish(),
+        cursor: z.string().nullish(),
+        userId: z.string(),
+        showOnlyUserRequests: z.boolean().nullish(),
+      }),
+    )
+    .query(async ({ input, ctx }) => {
+      const limit = input.limit ?? 15
+      const { cursor } = input
+
+      const items = await ctx.prisma.request.findMany({
+        where: {
+          user: {
+            ...(input.showOnlyUserRequests
+              ? {
+                  is: {
+                    id: input.userId,
+                  },
+                }
+              : {
+                  isNot: {
+                    id: input.userId,
+                  },
+                }),
+          },
+        },
+        take: limit + 1,
+        cursor: cursor ? { id: cursor } : undefined,
+      })
+
+      let nextCursor: typeof cursor | undefined = undefined
+      if (items.length > limit) {
+        const nextItem = items.pop()
+        nextCursor = nextItem?.id
+      }
+
+      return {
+        items,
+        nextCursor,
+      }
+    }),
 
   updateRequest: protectedProcedure
     .input(
