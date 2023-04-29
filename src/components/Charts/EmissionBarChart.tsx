4c4136b0c75f98bdd4d4040df4010ff7cd321cd6
anthony <anthony.cruzmacedo@protonmail.com>
2023-04-29 16:32:09 -0500

    Add colors to emissions bar chart

--- src/components/Charts/EmissionBarChart.tsx
+++ src/components/Charts/EmissionBarChart.tsx
@@ -10,6 +10,11 @@ interface EmissionBarChartProps {
 
 const EmissionBarChart: FC<EmissionBarChartProps> = ({ emissionData }) => {
   const labels = ['Scope 1 Estimate', 'Scope 2 Estimate', 'Scope 3 Estimate']
+  const colors = {
+    'Scope 1 Estimate': '#E6F0FA',
+    'Scope 2 Estimate': '#40D7D4',
+    'Scope 3 Estimate': '#0F81E8',
+  }
   const scopeValues = [
     emissionData?.scopeOne ?? 0,
     emissionData?.scopeTwo ?? 0,
@@ -19,6 +24,7 @@ const EmissionBarChart: FC<EmissionBarChartProps> = ({ emissionData }) => {
   const emissions = scopeValues.map((value, index) => ({
     x: labels[index],
     y: value,
+    fillColor: colors[labels[index]] as string,
   }))
 
   const series = [
