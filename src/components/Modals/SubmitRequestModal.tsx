9df8a57cf044d2f29b8ec5f245b0f0fabe2eb7e5
Liza George <liza@chiramattel.com>
2023-04-29 20:08:24 -0500

    Close modal on clicking outside

--- src/components/Modals/SubmitRequestModal.tsx
+++ src/components/Modals/SubmitRequestModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import type { FC } from 'react'
 import clsx from 'clsx'
 
@@ -10,31 +10,29 @@ import GeneralModal from './GeneralModal'
 // isOpen: boolean
 // }
 
-export const SubmitRequestModal: FC = (
-  {
-    // isOpen,
-  },
-) => {
+export const SubmitRequestModal: FC = ({}) => {
   const flexStyles = clsx('flex flex-col items-center')
+  const [isOpen, setIsOpen] = useState(true)
 
   return (
-    <GeneralModal
-      // isOpen={isOpen}
-      isOpen={true}
-      className={clsx(
-        'h-2/5 w-3/5 justify-evenly gap-2 py-4 sm:flex-[0_0_35%]',
-        flexStyles,
-      )}
-    >
-      <p className="header-3 w-4/5 text-center font-medium text-black ">
-        Submit Request
-      </p>
-      <PrimaryButton
-        text="Submit Request"
-        link={''}
-        className="flex items-center justify-center !px-14 !py-4 text-center !font-medium"
-      />
-    </GeneralModal>
+    <div onClick={() => setIsOpen(false)}>
+      <GeneralModal
+        isOpen={isOpen}
+        className={clsx(
+          'h-2/5 w-3/5 justify-evenly gap-2 py-4 sm:flex-[0_0_35%]',
+          flexStyles,
+        )}
+      >
+        <p className="header-3 w-4/5 text-center font-medium text-black ">
+          Submit Request
+        </p>
+        <PrimaryButton
+          text="Submit Request"
+          link={''}
+          className="flex items-center justify-center !px-14 !py-4 text-center !font-medium"
+        />
+      </GeneralModal>
+    </div>
   )
 }
 
