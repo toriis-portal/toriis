e664044a6c7e417967665deb44be00d98bf0bae2
Liza George <liza@chiramattel.com>
2023-04-29 20:32:57 -0500

    Modified GeneralModal for proper closing logic

--- src/components/Modals/GeneralModal.tsx
+++ src/components/Modals/GeneralModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import type { FC } from 'react'
 import clsx from 'clsx'
 
@@ -6,23 +6,34 @@ interface GeneralModalProps {
   isOpen: boolean
   className?: string
   children?: React.ReactNode
+  closeOnOutsideClick?: boolean
 }
 
 export const GeneralModal: FC<GeneralModalProps> = ({
   isOpen,
   className,
   children,
+  closeOnOutsideClick = false,
 }) => {
+  const [internalIsOpen, setInternalIsOpen] = useState(true)
+
   return (
     <>
-      {isOpen && (
-        <div className="fixed inset-0 z-10 flex h-full w-full items-center justify-center bg-[rgba(0,0,0,.5)]">
+      {(closeOnOutsideClick ? internalIsOpen : isOpen) && (
+        <div
+          className="fixed inset-0 z-10 flex h-full w-full items-center justify-center bg-[rgba(0,0,0,.5)]"
+          onClick={() => {
+            setInternalIsOpen(false)
+            console.log('closing')
+          }}
+        >
           <div
             className={clsx(
               'z-20 rounded-xl border-[1px]',
               'border-black bg-white',
               className,
             )}
+            onClick={(e) => e.stopPropagation()}
           >
             {children}
           </div>
--- src/components/Modals/SubmitRequestModal.tsx
+++ src/components/Modals/SubmitRequestModal.tsx
@@ -6,46 +6,40 @@ import PrimaryButton from '../Buttons/PrimaryButton'
 
 import GeneralModal from './GeneralModal'
 
-// interface ModalProps {
-// isOpen: boolean
-// }
-
 export const SubmitRequestModal: FC = ({}) => {
   const flexStyles = clsx('flex flex-col items-center')
-  const [isOpen, setIsOpen] = useState(true)
 
   return (
-    <div onClick={() => setIsOpen(false)}>
-      <GeneralModal
-        isOpen={isOpen}
-        className={clsx(
-          'w-3/5 justify-evenly gap-2 py-8 px-12 sm:flex-[0_0_35%]',
-          flexStyles,
-        )}
-      >
-        <p className="header-3 w-4/5 text-center font-medium text-black ">
-          Submit Request
+    <GeneralModal
+      isOpen={true}
+      closeOnOutsideClick={true}
+      className={clsx(
+        'justify-evenly gap-2 py-8 px-12 sm:flex-[0_0_35%]',
+        flexStyles,
+      )}
+    >
+      <p className="header-3 w-4/5 text-center font-medium text-black ">
+        Submit Request
+      </p>
+
+      <label htmlFor="comments">
+        <p>
+          Comments <span className="text-medGray">(Optional)</span>
         </p>
-
-        <label htmlFor="comments">
-          <p>
-            Comments <span className="text-medGray">(Optional)</span>
-          </p>
-          <input
-            type="text"
-            name="comments"
-            id="comments"
-            className="mb-4 block h-32 w-[507px] rounded"
-          />
-        </label>
-
-        <PrimaryButton
-          text="Submit Request"
-          link={''}
-          className="flex items-center justify-center !px-14 !py-4 text-center !font-medium"
+        <input
+          type="text"
+          name="comments"
+          id="comments"
+          className="mb-4 block h-32 w-[507px] rounded"
         />
-      </GeneralModal>
-    </div>
+      </label>
+
+      <PrimaryButton
+        text="Submit Request"
+        link={''}
+        className="mt-2 flex items-center justify-center !px-14 !py-4 text-center !font-medium"
+      />
+    </GeneralModal>
   )
 }
 
