1cec99ed7103d579186b89e38e698630b654b1f4
Liza George <liza@chiramattel.com>
2023-07-05 23:26:13 -0700

    Add navbars

--- src/pages/letter/index.tsx
+++ src/pages/letter/index.tsx
@@ -1,9 +1,29 @@
-import { PrimaryNavBar } from '../../components'
+import { PrimaryNavBar, SecondaryNavBar } from '../../components'
 
 const LetterPage = () => {
+  const navItems = [
+    { path: 'openLetter', text: 'Open Letter' },
+    { path: 'signLetter', text: 'Sign the Letter' },
+    { path: 'signatories', text: 'Signatories' },
+    { path: 'momentum', text: 'Continue the Momentum' },
+  ]
   return (
     <>
       <PrimaryNavBar />
+      <SecondaryNavBar navItems={navItems} />
+
+      <div id="openLetter" className="pt-10">
+        openLetter placeholder
+      </div>
+      <div id="signLetter" className="pt-10">
+        signLetter placeholder
+      </div>
+      <div id="signatories" className="pt-10">
+        signatories placeholder
+      </div>
+      <div id="momentum" className="pt-10">
+        momentum placeholder
+      </div>
     </>
   )
 }
