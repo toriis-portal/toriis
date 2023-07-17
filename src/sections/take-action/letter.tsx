47e897a019ff799461608031f4ced48b6680ea43
Liza George <liza@chiramattel.com>
2023-07-16 23:20:27 -0700

    Add basic signatories counter

--- src/sections/take-action/letter.tsx
+++ src/sections/take-action/letter.tsx
@@ -19,6 +19,13 @@ const OpenLetter: FC<LetterProps> = ({ openLetter }) => {
           color="clementine"
         />
       </div>
+
+      <div className="flex flex-row items-center">
+        <p className="font-semibold">Join our</p>
+        {/* TODO: get this number from contentful */}
+        <p className="px-6 text-8xl font-bold">709</p>
+        <p className="font-semibold">signatories</p>
+      </div>
       <div className="space-y-4 px-60">
         {documentToReactComponents(openLetter, mainParagraphStyle)}
       </div>
