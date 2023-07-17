126ecd06d4335f1c8c14213f93c9d955a14bcb82
Liza George <liza@chiramattel.com>
2023-07-17 00:18:59 -0700

    Start sign letter section, with box

--- src/sections/take-action/sign-letter.tsx
+++ src/sections/take-action/sign-letter.tsx
@@ -2,13 +2,16 @@ import { HighlightedTitle } from '../../components'
 
 const SignLetter = () => {
   return (
-    <div className="bg-clementine/20">
+    <div className="flex flex-col items-center bg-clementine/20 py-10">
       <div>
         <HighlightedTitle
           title="Sign the Letter"
           size="large"
           color="clementine"
         />
+      </div>
+      <div className="w-[calc(100%-120px)] rounded-lg py-9 px-20 outline outline-1">
+        form!
       </div>
     </div>
   )
