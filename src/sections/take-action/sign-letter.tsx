2e7cd59f235d3373c69234d4a8461ad6fab83a7d
Liza George <liza@chiramattel.com>
2023-07-17 00:34:56 -0700

    Add left section of sign section

--- src/sections/take-action/sign-letter.tsx
+++ src/sections/take-action/sign-letter.tsx
@@ -11,7 +11,38 @@ const SignLetter = () => {
         />
       </div>
       <div className="w-[calc(100%-120px)] rounded-lg py-9 px-20 outline outline-1">
-        form!
+        {/* TODO: need Graph FF Condensed font */}
+        <p className="header-3 text-center font-semibold">
+          FOSSIL FREE UIUC OPEN LETTER
+        </p>
+        <div className="flex">
+          <div className="flex w-1/2 flex-col items-center justify-center">
+            <p className="">
+              <span className="underline decoration-clementine">
+                Support Fossil Fuel Divestment
+              </span>{' '}
+              at the{' '}
+              <span className="underline decoration-clementine">
+                University of Illinois Urbana-Champaign
+              </span>{' '}
+              by signing the{' '}
+              <span className="underline decoration-clementine">
+                Fossil Free UIUC Open Letter
+              </span>
+              .
+            </p>
+            <div className="flex flex-row items-end">
+              {/* TODO: need Graph FF Condensed font */}
+              <p className="font-semibold">Join our</p>
+              {/* TODO: get this number from contentful */}
+              <p className="px-6 text-8xl font-bold">709</p>
+              <p className="font-semibold">
+                signatories today<span className="text-clementine">.</span>
+              </p>
+            </div>
+          </div>
+          <div className="h-[800px] w-1/2 bg-slate-100">form!</div>
+        </div>
       </div>
     </div>
   )
