31dcf0e9683bf7c669d9f76f64fababd652e3851
Liza George <liza@chiramattel.com>
2023-08-07 18:05:43 -0700

    Add banner image

--- src/sections/home/LandingBanner.tsx
+++ src/sections/home/LandingBanner.tsx
@@ -1,4 +1,16 @@
+import Image from 'next/image'
+
+import bannerimage from '../../../public/banner.jpeg'
+
 const LandingBanner = () => {
-  return <div>hi</div>
+  return (
+    <div>
+      <Image
+        src={bannerimage as HTMLImageElement}
+        className="w-auto"
+        alt="Protest background"
+      />
+    </div>
+  )
 }
 export default LandingBanner
