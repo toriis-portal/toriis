d06aa077fecd0e0e192e758f1093bf0307db057e
Liza George <liza@chiramattel.com>
2023-08-07 17:42:25 -0700

    Add yup and email validation

--- src/components/input/LetterForm.tsx
+++ src/components/input/LetterForm.tsx
@@ -1,4 +1,5 @@
 import { Formik, Field, Form } from 'formik'
+import * as Yup from 'yup'
 import clsx from 'clsx'
 
 import { PrimaryButton } from '..'
@@ -29,6 +30,11 @@ const LetterForm = () => {
     checkbox:
       'rounded-sm border border-black checked:bg-black bg-clementine/20',
   }
+
+  const ValidationSchema = Yup.object().shape({
+    email: Yup.string().email('Invalid email').required('Email required'),
+  })
+
   return (
     <Formik
       initialValues={{
@@ -44,6 +50,7 @@ const LetterForm = () => {
         agree: '',
         emailUpdates: true,
       }}
+      validationSchema={ValidationSchema}
       onSubmit={(values: Values) => {
         console.log(values)
         addSignatoryMutation.mutate({
@@ -62,119 +69,128 @@ const LetterForm = () => {
         })
       }}
     >
-      <Form>
-        {/* TODO: accessibility concerns of no label */}
-        <div className="flex space-x-4">
+      {({ errors, touched }) => (
+        <Form>
+          {/* TODO: accessibility concerns of no label */}
+          <div className="flex space-x-4">
+            <Field
+              id="firstName"
+              name="firstName"
+              placeholder="First"
+              className={clsx('w-full', styles.field)}
+            />
+            <Field
+              id="lastName"
+              name="lastName"
+              placeholder="Last"
+              className={clsx('w-full', styles.field)}
+            />
+          </div>
           <Field
-            id="firstName"
-            name="firstName"
-            placeholder="First"
+            id="title"
+            name="tile"
+            placeholder="Title"
             className={clsx('w-full', styles.field)}
           />
           <Field
-            id="lastName"
-            name="lastName"
-            placeholder="Last"
+            id="institution"
+            name="institution"
+            placeholder="Institution"
             className={clsx('w-full', styles.field)}
           />
-        </div>
-        <Field
-          id="title"
-          name="tile"
-          placeholder="Title"
-          className={clsx('w-full', styles.field)}
-        />
-        <Field
-          id="institution"
-          name="institution"
-          placeholder="Institution"
-          className={clsx('w-full', styles.field)}
-        />
-        <Field
-          id="email"
-          name="email"
-          placeholder="Email*"
-          type="email"
-          className={clsx('block', styles.field)}
-        />
-        <Field
-          id="city"
-          name="city"
-          placeholder="City"
-          className={clsx('block', styles.field)}
-        />
-        <Field
-          id="country"
-          name="country"
-          placeholder="Country"
-          className={clsx('block', styles.field)}
-        />
-        <Field
-          id="zip"
-          name="zip"
-          placeholder="Zip/Postal Code"
-          className={clsx('block', styles.field)}
-        />
-        <label className={styles.label} htmlFor="bio">
-          Link to Bio, University Webpage, Etc.
-        </label>
-        <Field
-          id="bio"
-          name="bio"
-          placeholder="Link Here"
-          className={clsx('block', styles.field)}
-        />
-        <label className={styles.label} htmlFor="bio">
-          Twitter Handle
-        </label>
-        <Field
-          id="twitter"
-          name="twitter"
-          placeholder="Link Here"
-          className={clsx('block', styles.field)}
-        />
-        <div>
-          <p className="mb-2 w-3/4 text-base">
-            I TESTIFY THAT THE INFORMATION I HAVE SUPPLIED ON THIS FORM IS TRUE
-            AND ACCURATELY REFLECTS MY IDENTITY, AS WELL AS DESIRE TO SIGN THE
-            FOSSIL FREE UIUC LETTER.
-          </p>
-          <label className="mb-2.5 block text-base">
-            <Field
-              type="radio"
-              name="agree"
-              value="YES"
-              className={clsx('mr-7 ml-1 ', styles.checkbox)}
-            />
-            YES
-          </label>
-          <label className="mb-2.5 block text-base">
-            <Field
-              type="radio"
-              name="agree"
-              value="NO"
-              className={clsx('mr-7 ml-1 ', styles.checkbox)}
-            />
-            NO
-          </label>
-        </div>
-        <div className="flex flex-col items-center">
-          <PrimaryButton
-            text="Add Your Name"
-            variant="clementine"
-            className="mt-2.5 mb-5 py-2 !px-14"
-            type="submit"
+          <Field
+            id="email"
+            name="email"
+            placeholder="Email*"
+            type="email"
+            className={clsx(
+              'block',
+              styles.field,
+              errors.email && touched.email && '!mb-1 ',
+            )}
           />
-          <label className="text-base">
-            <Field
-              type="checkbox"
-              name="emailUpdates"
-              className={clsx('mr-4', styles.checkbox)}
-            />
-            Opt in to email updates from TORIIS
+          {errors.email && touched.email ? (
+            <div className="mb-5 text-red-700">{errors.email}</div>
+          ) : null}
+          <Field
+            id="city"
+            name="city"
+            placeholder="City"
+            className={clsx('block', styles.field)}
+          />
+          <Field
+            id="country"
+            name="country"
+            placeholder="Country"
+            className={clsx('block', styles.field)}
+          />
+          <Field
+            id="zip"
+            name="zip"
+            placeholder="Zip/Postal Code"
+            className={clsx('block', styles.field)}
+          />
+          <label className={styles.label} htmlFor="bio">
+            Link to Bio, University Webpage, Etc.
           </label>
-        </div>
-      </Form>
+          <Field
+            id="bio"
+            name="bio"
+            placeholder="Link Here"
+            className={clsx('block', styles.field)}
+          />
+          <label className={styles.label} htmlFor="bio">
+            Twitter Handle
+          </label>
+          <Field
+            id="twitter"
+            name="twitter"
+            placeholder="Link Here"
+            className={clsx('block', styles.field)}
+          />
+          <div>
+            <p className="mb-2 w-3/4 text-base">
+              I TESTIFY THAT THE INFORMATION I HAVE SUPPLIED ON THIS FORM IS
+              TRUE AND ACCURATELY REFLECTS MY IDENTITY, AS WELL AS DESIRE TO
+              SIGN THE FOSSIL FREE UIUC LETTER.
+            </p>
+            <label className="mb-2.5 block text-base">
+              <Field
+                type="radio"
+                name="agree"
+                value="YES"
+                className={clsx('mr-7 ml-1 ', styles.checkbox)}
+              />
+              YES
+            </label>
+            <label className="mb-2.5 block text-base">
+              <Field
+                type="radio"
+                name="agree"
+                value="NO"
+                className={clsx('mr-7 ml-1 ', styles.checkbox)}
+              />
+              NO
+            </label>
+          </div>
+          <div className="flex flex-col items-center">
+            <PrimaryButton
+              text="Add Your Name"
+              variant="clementine"
+              className="mt-2.5 mb-5 py-2 !px-14"
+              type="submit"
+            />
+            <label className="text-base">
+              <Field
+                type="checkbox"
+                name="emailUpdates"
+                className={clsx('mr-4', styles.checkbox)}
+              />
+              Opt in to email updates from TORIIS
+            </label>
+          </div>
+        </Form>
+      )}
     </Formik>
   )
 }
