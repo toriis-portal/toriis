6214673960ccc70ef32818ab1691314a4886d30e
eeshabarua <eesha.barua@gmail.com>
2023-08-14 14:56:05 -0400

    Add Icons and Styling

--- src/components/card/EmissionsCard.tsx
+++ src/components/card/EmissionsCard.tsx
@@ -1,13 +1,66 @@
 import type { FC } from 'react'
+import {
+  TruckIcon,
+  TrashIcon,
+  BoltIcon,
+  GlobeAmericasIcon,
+} from '@heroicons/react/24/outline'
+import clsx from 'clsx'
 
 export interface CardProps {
   title: string
   metric: string
   description: string
+  color: 'clementine' | 'brightTeal'
+  type: 'gasoline' | 'trashbag' | 'home' | 'tree'
 }
 
-const EmissionsCard: FC<CardProps> = ({ title, metric, description }) => {
-  return <div>test</div>
+interface IconProps {
+  type: 'gasoline' | 'trashbag' | 'home' | 'tree'
+}
+
+const Icon: FC<IconProps> = ({ type }) => {
+  let icon
+
+  switch (type) {
+    case 'gasoline':
+      icon = <TruckIcon />
+      break
+    case 'trashbag':
+      icon = <TrashIcon />
+      break
+    case 'home':
+      icon = <BoltIcon />
+      break
+    case 'tree':
+      icon = <GlobeAmericasIcon />
+      break
+    default:
+      icon = <GlobeAmericasIcon />
+  }
+
+  return <div className="w-8"> {icon}</div>
+}
+
+const EmissionsCard: FC<CardProps> = ({
+  title,
+  metric,
+  description,
+  color,
+  type,
+}) => {
+  return (
+    <div className="p-8">
+      <h2 className="p-2">{title}:</h2>
+      <div className={clsx('rounded-md border-2 ', `border-${color}`)}>
+        <div className="flex place-content-evenly p-4">
+          <text className={clsx('w-24', `bg-${color}/20`)}>{metric}</text>
+          <text className="w-64">{description}</text>
+          <Icon type={type} />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default EmissionsCard
--- src/sections/fossil-fuel/UniversityInvestments.tsx
+++ src/sections/fossil-fuel/UniversityInvestments.tsx
@@ -9,6 +9,7 @@ import {
   EmissionTreeMap,
   PrimaryButton,
   ToolTip,
+  EmissionsCard,
 } from '../../components'
 import { TOOLTIP_DEFINITIONS } from '../../utils/constants'
 
@@ -68,13 +69,43 @@ const UniversityInvestments: FC<UniversityInvestmentsProps> = ({
           </div>
         </div>
         <div>{documentToReactComponents(caption, mainParagraphStyle)}</div>
-        <div className="justify-center">
+        <div className="flex justify-center py-10">
           <HighlightedTitle
             title="66,299 Metric Tons of CO2 is Equivalent to"
             size="medium"
             color="clementine"
             padded={false}
           ></HighlightedTitle>
+        </div>
+        <div className="grid grid-cols-2 gap-4">
+          <EmissionsCard
+            title="GREENHOUSE GAS EMISSIONS FROM"
+            metric="14,754"
+            description="gasoline powered vehicles driven for one year"
+            color="clementine"
+            type="gasoline"
+          />
+          <EmissionsCard
+            title="GREENHOUSE GAS EMISSIONS AVOIDED BY"
+            metric="2,869,795"
+            description="trashbags of waste recycled instead of landfilled"
+            color="clementine"
+            type="trashbag"
+          />
+          <EmissionsCard
+            title="CO2 EMISSIONS FROM"
+            metric="12,900"
+            description="homes' electricity use for one year"
+            color="brightTeal"
+            type="home"
+          />
+          <EmissionsCard
+            title="CARBON SEQUESTERED BY"
+            metric="1,096,260"
+            description="tree seedlings grown for 10 years"
+            color="brightTeal"
+            type="tree"
+          />
         </div>
       </div>
     </div>
