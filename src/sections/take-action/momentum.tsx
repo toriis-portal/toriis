de0cd59bc80c5899054ab2a53d186dbf1c4476cf
kennykos <gkosmacher01@gmail.com>
2023-08-14 09:37:28 -0500

    Change padding to reflect Figma

--- src/sections/take-action/momentum.tsx
+++ src/sections/take-action/momentum.tsx
@@ -7,14 +7,14 @@ import { mainParagraphStyle } from '../../utils/renderer'
 
 const Momentum: FC<{ entries: ListEntryDocument[] }> = ({ entries }) => {
   return (
-    <div className="px-12">
+    <div className="px-16 pb-36">
       <HighlightedTitle
         title="Continue the Momentum"
         size="large"
         color="clementine"
       />
-      <div className="my-6 w-full md:px-12">
-        <div className="ml-[3%] space-y-6">
+      <div className="my-6 mt-16 w-full md:px-12">
+        <div className="ml-[3%] w-4/6 space-y-6">
           {entries.map((entry, index) => (
             <ListItem
               key={index}
