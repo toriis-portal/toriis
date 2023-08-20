de36323f15fbe683ebca60795762e171a5ca58f9
zorazrr <zorazrr@gmail.com>
2023-08-19 20:15:08 -0500

    Delete unused

--- src/components/text/UnderlinedTitle.tsx
+++
@@ -1,9 +0,0 @@
-import type { FC, ReactNode } from 'react'
-
-const UnderlinedTitle: FC<{ children: ReactNode }> = ({ children }) => {
-  return (
-    <p className="underline decoration-clementine decoration-8">{children}</p>
-  )
-}
-
-export default UnderlinedTitle
